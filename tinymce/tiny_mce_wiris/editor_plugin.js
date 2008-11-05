33efa4e3982847b7270a7a61734d500fe427b963
CVS <cvs@wiris.com>
2008-11-05 10:19:33 +0000

    *** empty log message ***

---
+++ tinymce/tiny_mce_wiris/editor_plugin.js
@@ -0,0 +1,27 @@
+(function() {
+	tinymce.create('tinymce.plugins.tinyWIRIS', {
+		init : function (editor, url) {
+			editor.addCommand('wrs_addFormula', function() {
+				alert('omg');
+				wrs_int_openNewFormulaEditor(editor._iframe);
+			});
+
+			editor.addButton('addFormula', {
+				title: 'tinyWIRIS.tinyWIRIS_desc',
+				cmd: 'wrs_addFormula'
+			});
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'tinyWIRIS',
+				author : 'Juan Lao Tebar - Maths for More',
+				authorurl : 'http://www.wiris.com',
+				infourl : 'http://www.mathsformore.com',
+				version : '1.0'
+			};
+		}
+	});
+
+	tinymce.PluginManager.add('tinyWIRIS', tinymce.plugins.tinyWIRIS);
+})();
