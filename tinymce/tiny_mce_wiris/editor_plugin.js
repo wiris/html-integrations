e9ebcc03ba8687765e9adda66d7bcf0581fa7e59
CVS <cvs@wiris.com>
2008-11-10 11:13:17 +0000

    *** empty log message ***

--- tinymce/tiny_mce_wiris/editor_plugin.js
+++ tinymce/tiny_mce_wiris/editor_plugin.js
@@ -3,13 +3,14 @@
 		init : function (editor, url) {
 			editor.addCommand('wrs_addFormula', function() {
 				alert('omg');
-				wrs_int_openNewFormulaEditor(editor._iframe);
+				//wrs_int_openNewFormulaEditor(editor._iframe);
 			});
 
-			editor.addButton('addFormula', {
+			/*editor.addButton('addFormula', {
 				title: 'tinyWIRIS.tinyWIRIS_desc',
 				cmd: 'wrs_addFormula'
-			});
+			});*/
+			editor.addButton('emotions', {title : 'emotions.emotions_desc', cmd : 'wrs_addFormula'});
 		},
 
 		getInfo : function() {
