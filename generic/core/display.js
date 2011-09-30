c0709d9776271bd364bcc64eeb6c4b55e19041ac
CVS <cvs@wiris.com>
2011-09-30 15:47:23 +0000

    *** empty log message ***

--- generic/core/display.js
+++ generic/core/display.js
@@ -1,9 +1,21 @@
 if (window.attachEvent) {
 	window.attachEvent('onload', function () {
+		function isInAContentEditableElement(element) {
+			if (element == null) {
+				return false;
+			}
+			
+			if (element.contentEditable && element.contentEditable !== 'inherit') {
+				return true;
+			}
+			
+			return isInAContentEditableElement(element.parentNode);
+		}
+		
 		var images = document.getElementsByTagName('img');
 		
 		for (var i = images.length - 1; i >= 0; --i) {
-			if (images[i].className == 'Wirisformula') {
+			if (images[i].className == 'Wirisformula' && !isInAContentEditableElement(images[i])) {
 				images[i].align = '';
 				images[i].style.verticalAlign = (-images[i].height / 2) + 'px';
 			}
