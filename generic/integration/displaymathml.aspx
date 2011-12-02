b7a28002cf88d629819533fdd1c3077b30767640
Daniel Marques <dani@wiris.com>
2011-12-02 18:51:24 +0000

    *** empty log message ***

--- generic/integration/displaymathml.aspx
+++ generic/integration/displaymathml.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="displaymathml.aspx.cs" Inherits="displaymathml" %>
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeFile="displaymathml.aspx.cs" Inherits="displaymathml" %>
 var url = "<asp:Literal ID="Literal1" runat="server"></asp:Literal>";
 
 function generateEntities(s) {
@@ -86,3 +86,4 @@ if (window.attachEvent) {
 }
 
 window.addEventListener('load',mathmlFunction,false);
+
--- generic/integration/displaymathml.aspx.cs
+++ generic/integration/displaymathml.aspx.cs
@@ -15,6 +15,28 @@ public partial class displaymathml : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        this.Literal1.Text = this.Page.ResolveUrl("showimage.aspx");
+        Literal lit;
+        lit=(Literal)this.Controls[1];
+        lit.Text = this.Page.ResolveUrl("showimage.aspx");;
     }
+
+    #region Web Form Designer generated code
+    override protected void OnInit(EventArgs e)
+    {
+        //
+        // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+        //
+        InitializeComponent();
+        base.OnInit(e);
+    }
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.Load += new System.EventHandler(this.Page_Load);
+    }
+    #endregion
 }
