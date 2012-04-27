c9f074dcf44a99688bac009c0ee59f8b09610825
CVS <cvs@wiris.com>
2012-04-27 08:20:44 +0000

    *** empty log message ***

--- generic/integration/api.php
+++ generic/integration/api.php
@@ -30,24 +30,189 @@ class com_wiris_plugin_PluginAPI {
 	public function mathml2img($mathml, $baseURL, $properties = array()) {
 		$parsedProperties = array();
 		global $wrs_xmlFileAttributes;
-		
+
 		foreach ($properties as $key => $value) {
 			if (in_array($key, $wrs_xmlFileAttributes) || substr($key, 0, 4) == 'font') {
 				$parsedProperties[$key] = $value;
 			}
 		}
-		
+
 		$parsedProperties['mml'] = $mathml;
 		$toSave = wrs_createIni($parsedProperties);
 		$fileName = md5($toSave);
 		$url = $baseURL . '/showimage.php?formula=' . $fileName . '.png';
 		$filePath = $this->formulaDirectory . '/' . $fileName . '.ini';
-		
+
 		if (!is_file($filePath) && file_put_contents($filePath, $toSave) === false) {
 			throw new Exception('Unable to create formula file.');
 		}
-		
+
 		return $url;
 	}
+
+	private function initfilter($type){
+		global $CFG;
+		require_once($CFG->libdir . '/textlib.class.php');
+
+		if ($type == 'safeXML'){
+			$convertor = textlib_get_instance();
+			$this->TAGS->in_open = $convertor->convert('«', 'utf8');
+			$this->TAGS->in_close = $convertor->convert('»', 'utf8');
+			$this->TAGS->in_entity = $convertor->convert('§', 'utf8');
+			$this->TAGS->in_appletopen = $convertor->convert('«applet', 'utf8');
+			$this->TAGS->in_appletclose = $convertor->convert('«/applet»', 'utf8');			
+			$this->TAGS->in_mathopen = $convertor->convert('«math', 'utf8');
+			$this->TAGS->in_mathclose = $convertor->convert('«/math»', 'utf8');		
+			$this->TAGS->in_double_quote = $convertor->convert('¨', 'utf8');
+			$this->TAGS->out_open = '<';
+			$this->TAGS->out_close = '>';
+			$this->TAGS->out_entity = '&';
+			$this->TAGS->out_double_quote = '"';
+		}else if ($type == 'mathml'){
+			$this->TAGS->in_open = '<';
+			$this->TAGS->in_close = '>';
+			$this->TAGS->in_entity = '&';
+			$this->TAGS->in_appletopen = '<applet';
+			$this->TAGS->in_appletclose = '</applet>';			
+			$this->TAGS->in_mathopen = '<math';
+			$this->TAGS->in_mathclose = '</math>';		
+			$this->TAGS->in_double_quote = '"';
+			$this->TAGS->out_open = '<';
+			$this->TAGS->out_close = '>';
+			$this->TAGS->out_entity = '&';
+			$this->TAGS->out_double_quote = '"';
+		}
+	}        
+        
+	function filter_math($text, $type) {
+		$this->initfilter($type);
+		$output = ''; 
+		$n0 = 0;
+		// Search for '«math'. If it is not found, the
+		// content is returned without any modification
+		$n1 = stripos($text, $this->TAGS->in_mathopen);
+
+		if($n1 === false) {
+				return $text; // directly return the content
+		}
+
+		// filtering
+		while($n1 !== false) {
+			$output .= substr($text, $n0, $n1 - $n0);
+			$n0 = $n1;
+			$n1 = stripos($text, $this->TAGS->in_mathclose, $n0);
+			if(!$n1) {
+				break;
+			}
+			$n1 = $n1 + strlen($this->TAGS->in_mathclose);
+			// Getting the substring «math ... «/math>
+			$sub = substr($text, $n0, $n1 - $n0);
+
+			/*
+				* This filter does the following replacement inside the <math> tags.
+				*   <a href="<url>">blabla</a>  -->  <url>
+				* 
+				* The reason is that Moodle replaces URL's with HTML links ('A' tags) and ignores the <span class="nolink"> tag.
+				*/
+			$pattern = '/<a href="[^"]*" target="_blank">([^<]*)<\/a>/';
+			$replacement = '\1';
+			$sub = preg_replace($pattern, $replacement, $sub);
+
+			if ($type == 'safeXML'){
+				$sub = html_entity_decode($sub, ENT_COMPAT);
+				// replacing '¨' by '"'
+				$sub = str_replace($this->TAGS->in_double_quote, $this->TAGS->out_double_quote, $sub);			
+				// replacing '«' by '<'
+				$sub = str_replace($this->TAGS->in_open, $this->TAGS->out_open, $sub);
+				// replacing '»' by '>'
+				$sub = str_replace($this->TAGS->in_close, $this->TAGS->out_close, $sub);
+				// replacing '§' by '&'
+				$sub = str_replace($this->TAGS->in_entity, $this->TAGS->out_entity, $sub);
+			}
+
+			// generate the image code
+			$sub = $this->math_image($sub);                
+			
+			// appending the modified substring
+			$output .= $sub;
+			$n0 = $n1;
+			// searching next '«math'
+			$n1 = stripos($text, $this->TAGS->in_mathopen, $n0);
+		}
+		$output .= substr($text, $n0);
+		return $output;
+	}        
+
+	function filter_applet($text){
+		$this->initfilter('safeXML');
+		$output = ''; 
+		$n0 = 0;
+		// Search for '«applet'. If it is not found, the
+		// content is returned without any modification
+
+		$n1 = stripos($text, $this->TAGS->in_appletopen);
+
+		if($n1 === false) {
+				return $text; // directly return the content
+		}
+
+		// filtering
+		while($n1 !== false) {
+
+			$output .= substr($text, $n0, $n1 - $n0);
+
+			$n0 = $n1;
+			$n1 = stripos($text, $this->TAGS->in_appletclose, $n0);
+			if(!$n1) {
+					break;
+			}
+			$n1 = $n1 + strlen($this->TAGS->in_appletclose);
+			// Getting the substring «applet ... «/applet»
+			$sub = substr($text, $n0, $n1 - $n0);
+
+			/*
+				* This filter does the following replacement inside the <math> tags.
+				*   <a href="<url>">blabla</a>  -->  <url>
+				* 
+				* The reason is that Moodle replaces URL's with HTML links ('A' tags) and ignores the <span class="nolink"> tag.
+				*/
+			$pattern = '/<a href="[^"]*" target="_blank">([^<]*)<\/a>/';
+			$replacement = '\1';
+			$sub = preg_replace($pattern, $replacement, $sub);
+
+			// replacing '¨' by '"'
+			$sub = str_replace($this->TAGS->in_double_quote, $this->TAGS->out_double_quote, $sub);
+			// replacing '§' by '&'
+			$sub = str_replace($this->TAGS->in_entity, $this->TAGS->out_entity, $sub);
+			// replacing '«' by '<'
+			$sub = str_replace($this->TAGS->in_open, $this->TAGS->out_open, $sub);
+			// replacing '»' by '>'
+			$sub = str_replace($this->TAGS->in_close, $this->TAGS->out_close, $sub);
+
+			$output .= $sub;
+
+			$n0 = $n1;
+			// searching next '«applet'
+			$n1 = stripos($text, $this->TAGS->in_appletopen, $n0);
+		}
+		$output .= substr($text, $n0);
+		return $output;
+	}	
+        
+	/*
+	 * Generate the html IMG code corresponding to the specified MathML expression
+	 */
+	private function math_image($mathml) {
+		global $CFG;
+
+		include $CFG->dirroot . '/lib/editor/tinymce/version.php';
+
+		$src = $this->mathml2img($mathml, $CFG->wwwroot . "/lib/editor/tinymce/tiny_mce/" . $plugin->release . "/plugins/tiny_mce_wiris/integration");
+		$output = '<img align="middle" src="';
+		$output .= $src;
+		$output .= '" />'; 
+
+		return $output;
+	}        
 }
 ?>
