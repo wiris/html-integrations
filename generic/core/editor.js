1c3c7635acd13e7407e474be30a507556716671c
CVS <cvs@wiris.com>
2012-10-19 10:49:46 +0000

    *** empty log message ***

--- generic/core/editor.js
+++ generic/core/editor.js
@@ -131,10 +131,15 @@ wrs_int_opener.wrs_addEvent(window, 'load', function () {
 	controls.appendChild(cancelButton);
 
 	var manualLink = document.getElementById('a_manual');
-	if (strings['manual'] != null){
+	if (typeof manualLink != 'undefined' && strings['manual'] != null){
 		manualLink.innerHTML = strings['manual'];
 	}
 
+	var latexLink = document.getElementById('a_latex');
+	if (typeof latexLink != 'undefined' && strings['latex'] != null){
+		latexLink.innerHTML = strings['latex'];
+	}
+	
 	// Auto resizing.
 	setInterval(function () {
 		editorElement.style.height = (document.getElementById('container').offsetHeight - controls.offsetHeight - 10) + 'px';
--- generic/lang/en/strings.js
+++ generic/lang/en/strings.js
@@ -2,3 +2,4 @@ var strings = new Array();
 strings['cancel'] = '${plugin_cancel}';
 strings['accept'] = '${plugin_accept}';
 strings['manual'] = '${plugin_manual}';
+strings['latex'] = 'LaTeX';
