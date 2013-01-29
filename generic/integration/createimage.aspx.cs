df91d2180e1be1627aa08284027896512c740ce0
CVS <cvs@wiris.com>
2013-01-29 13:31:28 +0000

    *** empty log message ***

--- generic/integration/createimage.aspx.cs
+++ generic/integration/createimage.aspx.cs
@@ -14,42 +14,48 @@ namespace pluginwiris
 {
     public class createimage : System.Web.UI.Page
     {
+		//Page_Load with some configuration is launched twice
+		//The boolean load is to run it just once
+		bool load = true;
         private void Page_Load(object sender, System.EventArgs e)
         {
-            if (this.Request.Form["mml"] != null && this.Request.Form["mml"].Length > 0)
-			{
-				Hashtable properties = new Hashtable();
-				properties["mml"] = this.Request.Form["mml"];
-				
-				foreach (string key in this.Request.Form.AllKeys)
+			if (this.load){
+				if (this.Request.Form["mml"] != null && this.Request.Form["mml"].Length > 0)
 				{
-					if (key != null && (Libwiris.inArray(key, Libwiris.xmlFileAttributes) || (key.Length >= 4 && key.Substring(0, 4) == "font")))
+					Hashtable properties = new Hashtable();
+					properties["mml"] = this.Request.Form["mml"];
+					
+					foreach (string key in this.Request.Form.AllKeys)
 					{
-						properties[key] = this.Request.Form[key];
+						if (key != null && (Libwiris.inArray(key, Libwiris.xmlFileAttributes) || (key.Length >= 4 && key.Substring(0, 4) == "font")))
+						{
+							properties[key] = this.Request.Form[key];
+						}
 					}
+				
+					Hashtable config = Libwiris.loadConfig(this.MapPath(Libwiris.configFile));
+				
+					string toSave = Libwiris.createIni(properties);
+					string fileName = Libwiris.md5(toSave);
+					string url = this.Page.ResolveUrl("showimage.aspx") + "?formula=" + fileName + ".png";
+					string filePath = (Libwiris.getFormulaDirectory(config) != null) ? Libwiris.getFormulaDirectory(config) : this.MapPath(Libwiris.FormulaDirectory);
+					filePath += "/" + fileName + ".ini";
+
+					if (!File.Exists(filePath))
+					{
+						TextWriter file = new StreamWriter(filePath);
+						file.Write(toSave);
+						file.Close();
+					}
+
+					this.Response.Write(url);
 				}
-			
-				Hashtable config = Libwiris.loadConfig(this.MapPath(Libwiris.configFile));
-			
-				string toSave = Libwiris.createIni(properties);
-                string fileName = Libwiris.md5(toSave);
-                string url = this.Page.ResolveUrl("showimage.aspx") + "?formula=" + fileName + ".png";
-				string filePath = (Libwiris.getFormulaDirectory(config) != null) ? Libwiris.getFormulaDirectory(config) : this.MapPath(Libwiris.FormulaDirectory);
-				filePath += "/" + fileName + ".ini";
-
-                if (!File.Exists(filePath))
-                {
-                    TextWriter file = new StreamWriter(filePath);
-                    file.Write(toSave);
-                    file.Close();
-                }
-
-                this.Response.Write(url);
-            }
-            else
-            {
-                this.Response.Write("Error: no mathml has been sent.");
-            }
+				else
+				{
+					this.Response.Write("Error: no mathml has been sent.");
+				}			
+				this.load = false;
+			}
         }
 
         #region Web Form Designer generated code
