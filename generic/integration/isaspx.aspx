f700246ee4b9a95a04946c9f04fa90d668c65bb1
Daniel Marques <dani@wiris.com>
2013-04-19 13:14:03 +0000

    *** empty log message ***

---
+++ generic/integration/isaspx.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeFile="isaspx.aspx.cs" Inherits="pluginwiris.isaspx" %>
---
+++ generic/integration/isaspx.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace pluginwiris
+{
+    /// <summary>
+    /// Summary description for getmathml.
+    /// </summary>
+    public partial class isaspx : System.Web.UI.Page
+    {
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+            this.Response.Write("true");
+        }
+
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load += new System.EventHandler(this.Page_Load);
+        }
+        #endregion
+    }
+}
