c80e2f1a51163224c57bab0ed8099e4044fe063f
Daniel Marques <dani@wiris.com>
2014-01-16 10:24:09 +0000

    Fix WQFM-146

--- generic/integration/pluginbuilder.php
+++ generic/integration/pluginbuilder.php
@@ -11,6 +11,7 @@ if (file_exists($moodle2_4)) {
 	$wirisFilter = '../../../../../../../filter/wiris/MoodleConfigurationUpdater.php';
 	$config = '../../../../../../../config.php';
 	//if (file_exists($wirisFilter)) {
+		define('NO_MOODLE_COOKIES', true); // Because it interferes with caching
 		include_once $config;
 		include_once $wirisFilter;
 		$pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_configuration_MoodleConfigurationUpdater());
