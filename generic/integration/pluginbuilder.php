6ffca23b52c25c0b0e5a52355bd9417aa422d6f2
CVS <cvs@wiris.com>
2014-07-11 10:03:19 +0000

    Moodle path fixed.

--- generic/integration/pluginbuilder.php
+++ generic/integration/pluginbuilder.php
@@ -7,14 +7,19 @@ require_once 'lib/php/Boot.class.php';
 $pluginBuilder = com_wiris_plugin_api_PluginBuilder::getInstance();
 
 // Moodle base dir is 6 or 7 folders up, depending on the text editor.
-$moodle_dirrot = '../../../../../..';
+$moodle_dirrot = '../../../../../../../..';
 $moodle = false;
+
 if (file_exists($moodle_dirrot . '/lib/moodlelib.php')) {
     $moodle = true;
-} elseif (file_exists($moodle_dirrot . '/../lib/moodlelib.php')) {
-    $moodle_dirrot .= '/..';
+} elseif (file_exists(substr($moodle_dirrot, 0, -3) . '/../lib/moodlelib.php')) {
+    $moodle_dirrot .= substr($moodle_dirrot, 0, -3);
+    $moodle = true;
+} else if (file_exists(substr($moodle_dirrot, 0, -6) . '/../../lib/moodlelib.php')) {
+  	$moodle_dirrot .= substr($moodle_dirrot, 0, -6);
     $moodle = true;
 }
+
 if ($moodle) {
     $wirisFilter = $moodle_dirrot . '/filter/wiris/MoodleConfigurationUpdater.php';
     $config = $moodle_dirrot . '/config.php';	
