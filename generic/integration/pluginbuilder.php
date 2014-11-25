40a4caaf03c6f23087e34b40f5b9b309c1753d93
CVS <cvs@wiris.com>
2014-11-25 11:52:35 +0000

    PLUGINS-264: open-basedir bug

--- generic/integration/pluginbuilder.php
+++ generic/integration/pluginbuilder.php
@@ -7,17 +7,39 @@ require_once 'lib/php/Boot.class.php';
 $pluginBuilder = com_wiris_plugin_api_PluginBuilder::getInstance();
 
 // Moodle base dir is 6 or 7 folders up, depending on the text editor.
+// If open_basedir is defined, we only check moodlelib.php inside open_basedir environment
+//
 $moodle_dirrot = '../../../../../..';
 $moodle = false;
-if (file_exists($moodle_dirrot . '/lib/moodlelib.php')) {
-    $moodle = true;
-} elseif (file_exists($moodle_dirrot . '/../lib/moodlelib.php')) {
-    $moodle_dirrot .= '/..';
-    $moodle = true;
-}  elseif (file_exists($moodle_dirrot . '/../../lib/moodlelib.php')) {
-    $moodle_dirrot .= '/../..';
-    $moodle = true;
+// $deep = 10 allow to check all cases.
+$deep = 10;
+
+if ($openbasedir = get_cfg_var("open_basedir")) {
+    $openbasedirarray = explode(DIRECTORY_SEPARATOR, $openbasedir);
+    $currentscriptarray = explode(DIRECTORY_SEPARATOR, dirname(__FILE__));
+    $deep = count(array_diff($currentscriptarray, $openbasedirarray));
 }
+
+if ($deep >= 6) {
+    if (file_exists($moodle_dirrot . '/lib/moodlelib.php')) {
+        $moodle = true;
+    } else {
+        if ($deep >= 7) {
+            if (file_exists($moodle_dirrot . '/../lib/moodlelib.php')) {
+                $moodle_dirrot .= '/..';
+                $moodle = true;
+            } else {
+                if ($deep >= 8) {
+                    if (file_exists($moodle_dirrot . '/../../lib/moodlelib.php')) {
+                        $moodle_dirrot .= '/../..';
+                        $moodle = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
 if ($moodle) {
     $wirisFilter = $moodle_dirrot . '/filter/wiris/MoodleConfigurationUpdater.php';
     $config = $moodle_dirrot . '/config.php';
@@ -32,9 +54,9 @@ if ($moodle) {
         }
     include_once $config;
     include_once $wirisFilter;
-    
+
     $pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_configuration_MoodleConfigurationUpdater());
-    
+
 }
 
 $pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_web_PhpConfigurationUpdater());
