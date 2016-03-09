3f177d75e27091a7a746a6a4fef8fce77f5ef760
CVS <cvs@wiris.com>
2016-03-09 09:56:16 +0000

    WQFM-306

--- generic/integration/pluginbuilder.php
+++ generic/integration/pluginbuilder.php
@@ -8,28 +8,11 @@ $wrap->start();
 $pluginBuilder = com_wiris_plugin_api_PluginBuilder::getInstance();
 $wrap->stop();
 
-
-$moodle = false;
-
-// Check if platform is Moodle.
-if (file_exists(".." . DIRECTORY_SEPARATOR . "version.php")) { // Atto
-    $moodle=true;
-    $moodle_dirrot= $moodle_dirrot = "../../../../../.."; // Moodle 2.7 and upwards.
-}
-
-if (strrpos(getcwd(),"lib".DIRECTORY_SEPARATOR."editor".DIRECTORY_SEPARATOR."tinymce")) { // TinyMCE.
-    if (file_exists(".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "version.php")) { // Tinymce on Moodle 2.4 and upwards         .
-        $moodle=true;
-        $moodle_dirrot= "../../../../../../.."; // Moodle 2.4 and upwards.
-    } else {
-        $moodle = true;
-        $moodle_dirrot = "../../../../../../../.."; // Moodle 2.2 & 2.3.
-    }
-}
+$moodle = file_exists(".." . DIRECTORY_SEPARATOR . "version.php");
 
 if ($moodle) {
-    $wirisFilter = $moodle_dirrot . '/filter/wiris/MoodleConfigurationUpdater.php';
-    $config = $moodle_dirrot . '/config.php';
+    $wirisFilter = '../MoodleConfigurationUpdater.php';
+    $config = '../../../' . 'config.php';
 
     define('NO_MOODLE_COOKIES', true); // Because it interferes with caching
      $scriptName = explode('/', $_SERVER["SCRIPT_FILENAME"]);
