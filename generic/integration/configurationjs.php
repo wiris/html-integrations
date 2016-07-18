c4df39f8965603dae82dcf85cdb8d1e7d4d1baef
CVS <cvs@wiris.com>
2016-07-18 14:04:49 +0000

    WQFM-358: config.php Changes Content Type. Setting header after config.php is loaded.

--- generic/integration/configurationjs.php
+++ generic/integration/configurationjs.php
@@ -2,9 +2,6 @@
 
 // ${license.statement}
 
-header('Content-Type: application/x-javascript');
-$NL = "\r\n";
-
 // Loaded from configuration
 require_once 'pluginbuilder.php';
 
@@ -13,6 +10,8 @@ $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : "";
 $res = new com_wiris_system_service_HttpResponse();
 $pluginBuilder->addCorsHeaders($res, $origin);
 
+header('Content-Type: application/x-javascript');
+$NL = "\r\n";
 
 // Scripts
 echo   "// Scripts".$NL;
