de1ef29e2a68f1b3db2cc565dbb02eda9a0334b5
CVS <cvs@wiris.com>
2016-10-17 15:17:42 +0000

    WQFM-381

--- generic/integration/pluginbuilder.php
+++ generic/integration/pluginbuilder.php
@@ -2,6 +2,7 @@
 
 // ${license.statement}
 require_once 'plugin.php';
+require_once 'moodletreestorageandcache.php';
 
 $wrap = com_wiris_system_CallWrapper::getInstance();
 $wrap->start();
@@ -14,25 +15,35 @@ if ($moodle) {
     $wirisFilter = '../MoodleConfigurationUpdater.php';
     $config = '../../../' . 'config.php';
 
-    define('NO_MOODLE_COOKIES', true); // Because it interferes with caching
+   // define('NO_MOODLE_COOKIES', true); // Because it interferes with caching
      $scriptName = explode('/', $_SERVER["SCRIPT_FILENAME"]);
         $scriptName = array_pop($scriptName);
 
         if ($scriptName == 'showimage.php') {
             define('ABORT_AFTER_CONFIG', true);
-            define('MOODLE_INTERNAL', true); // Moodle 2.2 - 2.5 min config doesn't define 'MOODLE_INTERNAL'.
+            if (!defined('MOODLE_INTERNAL')) {
+                define('MOODLE_INTERNAL', true); // Moodle 2.2 - 2.5 min config doesn't define 'MOODLE_INTERNAL'.
+            }
         }
 
     $wrap->stop();
-    include_once $config;
-    include_once $wirisFilter;
+    if (!defined('MOODLE_INTERNAL')) {
+        require_once $config;
+    }
+    require_once $wirisFilter;
     $wrap->start();
     $pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_configuration_MoodleConfigurationUpdater());
     require_once('moodleparamsprovider.php');
     $pluginBuilder->setCustomParamsProvider(new MoodleParamsProvider());
+    $pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_web_PhpConfigurationUpdater());
+    $pluginBuilder->getConfiguration()->getFullConfiguration();
+    $storage = new MoodleStorageAndCache();
+    $storage->init(null, $pluginBuilder->getConfiguration()->getFullConfiguration());
+    $pluginBuilder->setStorageAndCache($storage);
 } else {
     $wrap->start();
     require_once('phpparamsprovider.php');
     $pluginBuilder->setCustomParamsProvider(new PhpParamsProvider());
+    $pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_web_PhpConfigurationUpdater());
 }
-$pluginBuilder->addConfigurationUpdater(new com_wiris_plugin_web_PhpConfigurationUpdater());
+
