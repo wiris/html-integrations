6a4f70bb37394497f8a2a6b1bc46190cce278907
Diego Lao <diego@wiris.com>
2017-03-08 08:20:23 +0000

    PLUGINS-688: Patch to pass codechecker.

--- generic/core/modal.js
+++ generic/core/modal.js
@@ -466,27 +466,26 @@ ModalWindow.prototype.stopDrag = function(ev) {
  * @ignore
  */
 ModalWindow.prototype.hideKeyboard = function() {
-    //creating temp field
+    // ...creating temp field.
     var field = document.createElement('input');
     field.setAttribute('type', 'text');
-    //hiding temp field from peoples eyes
-    //-webkit-user-modify is nessesary for Android 4.x
+    // ...hiding temp field from peoples eyes.
+    // ...-webkit-user-modify is nessesary for Android 4.x.
     field.setAttribute('style', 'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;');
     document.body.appendChild(field);
 
-    //adding onfocus event handler for out temp field
+    // ...adding onfocus event handler for out temp field.
     field.onfocus = function(){
-      //this timeout of 200ms is nessasary for Android 2.3.x
-      setTimeout(function() {
+          // ...this timeout of 200ms is nessasary for Android 2.3.x.
+          setTimeout(function() {
 
-        field.setAttribute('style', 'display:none;');
-        setTimeout(function() {
-          document.body.removeChild(field);
-          document.body.focus();
-        }, 14);
+                field.setAttribute('style', 'display:none;');
+                setTimeout(function() {
+                    document.body.removeChild(field);
+                    document.body.focus();
+                }, 14);
 
-      }, 200);
+          }, 200);
     };
-    //focusing it
     field.focus();
 }
