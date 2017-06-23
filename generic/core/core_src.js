540d331824efd6736d16198886436f61446b95ff
Manuel Cagigas <manuel@wiris.com>
2017-06-23 12:46:21 +0000

    PLUGINS-759: Use of standard URI Component decode algorithm. No single quotes (') encoding longer needed.

--- generic/core/core_src.js
+++ generic/core/core_src.js
@@ -2576,7 +2576,7 @@ function wrs_urldecode(input) {
 function wrs_urlencode(clearString) {
     var output = '';
     // Method encodeURIComponent doesn't encode !'()*~ .
-    output = encodeURIComponent(clearString).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/~/g, '%7E');
+    output = encodeURIComponent(clearString);
     return output;
 }
 
