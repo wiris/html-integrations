56c69fdfb47b06b3a96140056c84f76ee88d008a
Manuel Cagigas <manuel@wiris.com>
2018-07-03 10:39:19 +0000

    PLUGINS-1000: Popup not working on mobile devices fixed

--- packages/mathtype-html-integration-devkit/modal.js
+++ packages/mathtype-html-integration-devkit/modal.js
@@ -138,6 +138,26 @@ class ModalWindow {
         this.lastImageWasNew = true;
 
         this.contentManager = null;
+
+        // Overlay popup.
+        var popupStrings = {
+            'cancelString' : _wrs_stringManager.getString('cancel'),
+            'submitString' : _wrs_stringManager.getString('close'),
+            'message' : _wrs_stringManager.getString('close_modal_warning')
+        };
+
+        var callbacks = {
+            'closeCallback' : function(){this.close()}.bind(this),
+            'cancelCallback'  : function(){this.focus()}.bind(this)
+        }
+
+        var popupupProperties = {
+            'overlayElement' : this.container,
+            'callbacks' :callbacks,
+            'strings': popupStrings
+        }
+
+        this.popup = new PopUpMessage(popupupProperties);
     }
     /**
      * This method sets the contentElement object. A contentElement object
@@ -278,25 +298,6 @@ class ModalWindow {
             if (_wrs_conf_modalWindowFullScreen) {
                 this.maximize();
             }
-            // Overlay popup.
-            var popupStrings = {
-                'cancelString' : _wrs_stringManager.getString('cancel'),
-                'submitString' : _wrs_stringManager.getString('close'),
-                'message' : _wrs_stringManager.getString('close_modal_warning')
-            };
-
-            var callbacks = {
-                'closeCallback' : function(){this.close()}.bind(this),
-                'cancelCallback'  : function(){this.focus()}.bind(this)
-            }
-
-            var popupupProperties = {
-                'overlayElement' : this.container,
-                'callbacks' :callbacks,
-                'strings': popupStrings
-            }
-
-            this.popup = new PopUpMessage(popupupProperties);
         }
         else if (this.deviceProperties['isAndroid']) {
             this.createModalWindowAndroid();
