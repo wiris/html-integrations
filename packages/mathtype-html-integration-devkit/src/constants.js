7961e4be634c11fdf3a33a9226b547eae5e871f4
Manuel Cagigas <manuel@wiris.com>
2018-08-30 15:24:45 +0200

    PLUGINS-1077: Use property instead of attribute accesors for safeMathML and MathML objects

--- packages/mathtype-html-integration-devkit/src/constants.js
+++ packages/mathtype-html-integration-devkit/src/constants.js
@@ -4,35 +4,6 @@
  */
 export default class Constants {
     /**
-     * Standard XML special characters.
-     */
-    static get xmlCharacters() {
-        return {
-            'tagOpener': '<',       // Hex: \x3C.
-            'tagCloser': '>',       // Hex: \x3E.
-            'doubleQuote': '"',     // Hex: \x22.
-            'ampersand': '&',       // Hex: \x26.
-            'quote': '\''           // Hex: \x27.
-        };
-    }
-
-    /**
-     * Safe XML special characters. This characters are used instead the standard
-     * the standard to parse the  MathML if safeXML save mode is enable. Each XML
-     * special character have a UTF-8 representation.
-     */
-    static get safeXmlCharacters() {
-        return {
-            'tagOpener': '«',       // Hex: \xAB.
-            'tagCloser': '»',       // Hex: \xBB.
-            'doubleQuote': '¨',     // Hex: \xA8.
-            'ampersand': '§',       // Hex: \xA7.
-            'quote': '`',           // Hex: \x60.
-            'realDoubleQuote': '¨'
-        };
-    }
-
-    /**
      * Safe XML entities.
      */
     static get safeXmlCharactersEntities() {
@@ -60,3 +31,30 @@ export default class Constants {
         }
     }
 }
+/**
+ * Standard XML special characters.
+ */
+Constants.xmlCharacters =
+    {
+        'tagOpener': '<',       // Hex: \x3C.
+        'tagCloser': '>',       // Hex: \x3E.
+        'doubleQuote': '"',     // Hex: \x22.
+        'ampersand': '&',       // Hex: \x26.
+        'quote': '\''           // Hex: \x27.
+    }
+
+/**
+ * Safe XML special characters. This characters are used instead the standard
+ * the standard to parse the  MathML if safeXML save mode is enable. Each XML
+ * special character have a UTF-8 representation.
+ */
+Constants.safeXmlCharacters =
+    {
+        'tagOpener': '«',       // Hex: \xAB.
+        'tagCloser': '»',       // Hex: \xBB.
+        'doubleQuote': '¨',     // Hex: \xA8.
+        'ampersand': '§',       // Hex: \xA7.
+        'quote': '`',           // Hex: \x60.
+        'realDoubleQuote': '¨'
+    }
+
