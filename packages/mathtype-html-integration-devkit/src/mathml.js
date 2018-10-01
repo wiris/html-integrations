3711c3016c3c3268c0ebca15f7ea623dd52fd305
Diego Lao <diego@wiris.com>
2018-10-01 15:36:31 +0200

    PLUGINS-1091: Remove mathml function isEmpty

--- packages/mathtype-html-integration-devkit/src/mathml.js
+++ packages/mathtype-html-integration-devkit/src/mathml.js
@@ -337,36 +337,4 @@ export default class MathML {
             }
         }
     }
-
-    /**
-     * Returns true if mathml is empty. Otherwise, false.
-     * @param {string} mathml - valid MathML with standard XML tags.
-     * @returns {boolean} - true if mathml is empty. Otherwise, false.
-     */
-    static isEmpty(mathml) {
-        // MathML can have the shape <math></math> or '<math />'.
-        const closeTag = '>';
-        const closeTagInline = '/>';
-        const firstCloseTagIndex = mathml.indexOf(closeTag);
-        const firstCloseTagInlineIndex = mathml.indexOf(closeTagInline);
-        let empty = false;
-        // MathML is always empty in the second shape.
-        if (firstCloseTagInlineIndex !== -1) {
-            if (firstCloseTagInlineIndex === firstCloseTagIndex - 1) {
-                empty = true;
-            }
-        }
-
-        // MathML is always empty in the first shape when there aren't elements
-        // between math tags.
-        if (!empty) {
-            const mathTagEndRegex = new RegExp('</(.+:)?math>');
-            const mathTagEndArray = mathTagEndRegex.exec(mathml);
-            if (mathTagEndArray) {
-                empty = firstCloseTagIndex + 1 === mathTagEndArray.index;
-            }
-        }
-
-        return empty;
-    }
 }
