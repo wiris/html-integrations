fab49bbda55898785707d98dc68c2e186d73b348
Xavier Ripoll <xavier@wiris.com>
2019-01-23 11:14:54 +0100

    PLUGINS-994: Remove annotation tags from output HTML

--- ckeditor5/converters/mathmldataprocessor.js
+++ ckeditor5/converters/mathmldataprocessor.js
@@ -2,6 +2,8 @@ import XmlDataProcessor from '@ckeditor/ckeditor5-engine/src/dataprocessor/xmlda
 import { BR_FILLER } from '@ckeditor/ckeditor5-engine/src/view/filler';
 import DomConverter from '@ckeditor/ckeditor5-engine/src/view/domconverter';
 
+import MathML from '../core/src/mathml.js';
+
 export default class MathmlDataProcessor extends XmlDataProcessor {
 
     /**
@@ -53,7 +55,44 @@ export default class MathmlDataProcessor extends XmlDataProcessor {
         const container = doc.createElement( 'div' );
         container.appendChild( domFragment );
 
-        return container.innerHTML.replace( /&amp;/g, '&' );
+        // Cleaned HTML containing MathML
+        // 'abc<math>123</math>def<math>456</math>ghi'
+        let html = container.innerHTML.replace( /&amp;/g, '&' );
+
+        const mathPattern = /<math.*?<\/math.*?>/g;
+
+        // HTML broken down into pieces like so:
+        let htmlPieces = html.split( mathPattern ); // 'abc', 'def', 'ghi'
+        let mathmlPieces = html.match( mathPattern ); // '<math>123</math>', '<math>456</math>'
+
+        // Variables used in the loop
+        let open, inner, close; // '<math>', '123', '</math>'
+        let innerStartIndex, closeStartIndex; // Index of inner and close
+
+        // Iterate only through the <math>...</math> blocks
+        for ( let i = 0; i < mathmlPieces.length; i++ ) {
+
+            innerStartIndex = mathmlPieces[ i ].indexOf( '>' ) + 1;
+            closeStartIndex = mathmlPieces[ i ].indexOf( '</math', innerStartIndex );
+
+            open = mathmlPieces[ i ].substring( 0, innerStartIndex );
+            inner = mathmlPieces[ i ].substring( innerStartIndex, closeStartIndex );
+            close = mathmlPieces[ i ].substring( closeStartIndex );
+
+            // Remove the annotation tags introduced by "Hand"
+            inner = MathML.removeAnnotation( inner, 'application/json' );
+
+            mathmlPieces[ i ] = open + inner + close;
+
+        }
+
+        // Return joined pieces
+        return htmlPieces // [ 'abc', 'def', 'ghi' ]
+            .map( ( piece, index ) => [ piece, mathmlPieces[ index ] ] ) // [ [ 'abc', '<math>123</math>' ], [ 'def', '<math>456</math>' ], [ 'ghi', undefined ] ]
+            .flat() // [ 'abc', '<math>123</math>', 'def', '<math>456</math>', 'ghi', undefined ]
+            .filter( piece => typeof piece !== 'undefined' ) // [ 'abc', '<math>123</math>', 'def', '<math>456</math>', 'ghi' ]
+            .reduce( ( acc, cur ) => acc + cur ); // 'abc<math>123</math>def<math>456</math>ghi'
+
     }
 
 }
