62cd19855a6be91a95952e3b57080e0c8bb4c1a4
Xavier Ripoll <xavier@wiris.com>
2019-02-25 15:10:38 +0100

    PLUGINS-994: Re-add BR filler

--- packages/ckeditor5-mathml/src/conversion/mathmldataprocessor.js
+++ packages/ckeditor5-mathml/src/conversion/mathmldataprocessor.js
@@ -1,6 +1,24 @@
 import XmlDataProcessor from '@ckeditor/ckeditor5-engine/src/dataprocessor/xmldataprocessor';
+import { BR_FILLER } from '@ckeditor/ckeditor5-engine/src/view/filler';
+import DomConverter from '@ckeditor/ckeditor5-engine/src/view/domconverter';
 
 export default class MathmlDataProcessor extends XmlDataProcessor {
+
+    /**
+     * Creates a new instance of the MathML data processor class.
+     *
+     * @param {Object} options Configuration options.
+     * @param {Array<String>} [options.namespaces=[]] A list of namespaces allowed to use in the XML input.
+     */
+    constructor( options = {} ) {
+
+        // Call XmlDataProcessor's constructor
+        super( options );
+
+        // Use BR_FILLER instead of NSBP_FILLER to identify <br/> and remove them when retrieving data
+        this._domConverter = new DomConverter( { blockFiller: BR_FILLER } );
+
+    }
 
 	toData( viewFragment ) {
         // Convert view DocumentFragment to DOM DocumentFragment.
@@ -9,6 +27,11 @@ export default class MathmlDataProcessor extends XmlDataProcessor {
         let frontier = [ ...domFragment.children ];
         let current;
         while ( current = frontier.pop() ) {
+
+            // Removes any block fillers CKEditor might've added
+            if ( current.nodeName.toLowerCase() == 'br' && current.hasAttribute( 'data-cke-filler' ) && current.closest( 'math' ) ) {
+                current.parentNode.removeChild( current );
+            }
 
             // Replaces < with &lt;, > with &gt;, & with &amp; in attributes
             for ( let attr of current.attributes ) {
