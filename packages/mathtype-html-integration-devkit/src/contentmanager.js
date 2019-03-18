1e7e808d799812d12d9ea32d170b272b9f896bdb
Manuel Cagigas <manuel@wiris.com>
2019-03-18 11:57:05 +0100

    PLUGINS-1175: Fix code style for ContentManager class

--- packages/mathtype-html-integration-devkit/src/contentmanager.js
+++ packages/mathtype-html-integration-devkit/src/contentmanager.js
@@ -1,16 +1,18 @@
-import Configuration from './configuration.js';
-import EditorListener from './editorlistener.js';
-import Listeners from './listeners.js';
-import MathML from './mathml.js';
-import Util from './util.js';
+import Configuration from './configuration';
+import EditorListener from './editorlistener';
+import Listeners from './listeners';
+import MathML from './mathml';
+import Util from './util';
 
 /**
  * This class represents the content of a {@link ModalDialog} class, managing the following:
  * - The insertion of content into the current instance of the {@link ModalDialog} class.
- * - The actions to be done once the modal object has been submitted ({@link ContentManager.submitAction} method).
- * - The update of the content when the {@link ModalDialog} class is also updated, for example when ModalDialog
- *   is re-opened.
- * - The communication between the {@link ModalDialog} class and itself, if the content has been changed ({@link ContentManager.hasChanges} method).
+ * - The actions to be done once the modal object has been submitted
+ *   ({@link ContentManager.submitAction} method).
+ * - The update of the content when the {@link ModalDialog} class is also updated,
+ *   for example when ModalDialog is re-opened.
+ * - The communication between the {@link ModalDialog} class and itself, if the content
+ *   has been changed ({@link ContentManager.hasChanges} method).
  */
 export default class ContentManager {
   /**
@@ -28,7 +30,7 @@ export default class ContentManager {
     if ('editorAttributes' in contentManagerAttributes) {
       this.editorAttributes = contentManagerAttributes.editorAttributes;
     } else {
-      throw new Error('ContentManager constructor error: editorAttributes property missed.')
+      throw new Error('ContentManager constructor error: editorAttributes property missed.');
     }
 
     /**
@@ -88,8 +90,8 @@ export default class ContentManager {
      * @type {DeviceProperties}
      */
     this.deviceProperties = {};
-    this.deviceProperties.isAndroid = this.ua.indexOf("android") > -1;
-    this.deviceProperties.isIOS = ((this.ua.indexOf("ipad") > -1) || (this.ua.indexOf("iphone") > -1));
+    this.deviceProperties.isAndroid = this.ua.indexOf('android') > -1;
+    this.deviceProperties.isIOS = ((this.ua.indexOf('ipad') > -1) || (this.ua.indexOf('iphone') > -1));
 
     /**
      * Custom editor toolbar.
@@ -98,7 +100,8 @@ export default class ContentManager {
     this.toolbar = null;
 
     /**
-     * Instance of the {@link ModalDialog} class associated with the current {@link ContentManager} instance.
+     * Instance of the {@link ModalDialog} class associated with the current
+     * {@link ContentManager} instance.
      * @type {ModalDialog}
      */
     this.modalDialogInstance = null;
@@ -143,7 +146,8 @@ export default class ContentManager {
   }
 
   /**
-   * Sets an instance of {@link IntegrationModel} class to the current {@link ContentManager} instance.
+   * Sets an instance of {@link IntegrationModel} class to the current {@link ContentManager}
+   * instance.
    * @param {IntegrationModel} integrationModel - The {@link IntegrationModel} instance.
    */
   setIntegrationModel(integrationModel) {
@@ -173,9 +177,10 @@ export default class ContentManager {
    * editor's JavaScript is loaded.
    */
   insertEditor() {
-    // To know if editor JavaScript is loaded we need to wait until com.wiris.jsEditor namespace is ready.
+    // To know if editor JavaScript is loaded we need to wait until com.wiris.jsEditor
+    // namespace is ready.
     if ('com' in window && 'wiris' in window.com && 'jsEditor' in window.com.wiris) {
-      this.editor = com.wiris.jsEditor.JsEditor.newInstance(this.editorAttributes);
+      this.editor = window.com.wiris.jsEditor.JsEditor.newInstance(this.editorAttributes);
       this.editor.insertInto(this.modalDialogInstance.contentContainer);
       this.editor.focus();
       if (this.modalDialogInstance.rtl) {
@@ -190,9 +195,9 @@ export default class ContentManager {
       this.editor.getEditorModel().addEditorListener(this.editorListener);
 
       // iOS events.
-      if (this.modalDialogInstance.deviceProperties['isIOS']) {
-        setTimeout(function () { this.modalDialogInstance.hideKeyboard() }, 400);
-        var formulaDisplayDiv = document.getElementsByClassName('wrs_formulaDisplay')[0];
+      if (this.modalDialogInstance.deviceProperties.isIOS) {
+        setTimeout(function hide() { this.modalDialogInstance.hideKeyboard(); }, 400);
+        const formulaDisplayDiv = document.getElementsByClassName('wrs_formulaDisplay')[0];
         Util.addEvent(formulaDisplayDiv, 'focus', this.modalDialogInstance.handleOpenedIosSoftkeyboard);
         Util.addEvent(formulaDisplayDiv, 'blur', this.modalDialogInstance.handleClosedIosSoftkeyboard);
       }
@@ -209,38 +214,31 @@ export default class ContentManager {
    * Initializes the current class by loading MathType script.
    */
   init() {
-    var queryParams = window.location.search.substring(1).split("&");
-    for (var i = 0; i < queryParams.length; i++) {
-      var pos = queryParams[i].indexOf("v=");
-      if (pos >= 0) {
-        version = queryParams[i].substring(2);
-      }
-    }
-
-    var script = document.createElement('script');
+    const script = document.createElement('script');
     script.type = 'text/javascript';
-    var editorUrl = Configuration.get('editorUrl');
+    let editorUrl = Configuration.get('editorUrl');
     // We create an object url for parse url string and work more efficiently.
-    var urlObject = document.createElement('a');
+    const urlObject = document.createElement('a');
     urlObject.href = editorUrl;
 
     // Change to https if necessary.
-    if (window.location.href.indexOf("https://") == 0) {
-      // It check if browser is https and configuration is http. If this is so, we will replace protocol.
-      if (urlObject.protocol == 'http:') {
+    if (window.location.href.indexOf('https://') === 0) {
+      // It check if browser is https and configuration is http.
+      // If this is so, we will replace protocol.
+      if (urlObject.protocol === 'http:') {
         urlObject.protocol = 'https:';
       }
     }
 
     // Check protocol and remove port if it's standard.
-    if (urlObject.port == '80' || urlObject.port == '443') {
-      editorUrl = urlObject.protocol + '//' + urlObject.hostname + '/' + urlObject.pathname;
+    if (urlObject.port === '80' || urlObject.port === '443') {
+      editorUrl = `${urlObject.protocol}//${urlObject.hostname}/${urlObject.pathname}`;
     } else {
-      editorUrl = urlObject.protocol + '//' + urlObject.hostname + ':' + urlObject.port + '/' + urlObject.pathname;
+      editorUrl = `${urlObject.protocol}//${urlObject.hostname}:${urlObject.port}/${urlObject.pathname}`;
     }
 
     // Editor stats. Use environment property to set it.
-    var stats = {};
+    const stats = {};
     if ('editor' in this.environment) {
       stats.editor = this.environment.editor;
     } else {
@@ -260,11 +258,7 @@ export default class ContentManager {
     }
 
     // Load editor URL. We add stats as GET params.
-    script.src = editorUrl +
-      "?lang=" + this.language +
-      '&stats-editor=' + stats.editor +
-      '&stats-mode=' + stats.mode +
-      '&stats-version=' + stats.version;
+    script.src = `${editorUrl}?lang=${this.language}&stats-editor=${stats.editor}&stats-mode=${stats.mode}&stats-version=${stats.version}`;
 
     document.getElementsByTagName('head')[0].appendChild(script);
   }
@@ -281,7 +275,8 @@ export default class ContentManager {
   /**
    * Sets a MathML into {@link ContentManager.editor} instance.
    * @param {String} mathml - MathML string.
-   * @param {Boolean} focusDisabled - If true editor don't get focus after the MathML is set. False by default.
+   * @param {Boolean} focusDisabled - If true editor don't get focus after the MathML is set.
+   * False by default.
    */
   setMathML(mathml, focusDisabled) {
     // By default focus is enabled.
@@ -290,13 +285,14 @@ export default class ContentManager {
     }
     // Using setMathML method is not a change produced by the user but for the API
     // so we set to false the contentChange property of editorListener.
-    this.editor.setMathMLWithCallback(mathml, function () {
+    this.editor.setMathMLWithCallback(mathml, () => {
       this.editorListener.setWaitingForChanges(true);
-    }.bind(this));
+    });
+
     // We need to wait a little until the callback finish.
-    setTimeout(function () {
+    setTimeout(() => {
       this.editorListener.setIsContentChanged(false);
-    }.bind(this), 500);
+    }, 500);
 
     // In some scenarios - like closing modal object - editor mustn't be focused.
     if (!focusDisabled) {
@@ -315,25 +311,26 @@ export default class ContentManager {
   }
 
   /**
-   * Updates the edition area by calling {@link IntegrationModel.updateFormula}. Triggered by {@link ModalDialog.submitAction}.
+   * Updates the edition area by calling {@link IntegrationModel.updateFormula}.
+   * Triggered by {@link ModalDialog.submitAction}.
    */
   submitAction() {
     if (!this.editor.isFormulaEmpty()) {
       let mathML = this.editor.getMathMLWithSemantics();
       // Add class for custom editors.
-      if (this.customEditors.getActiveEditor() != null) {
-        mathML = MathML.addCustomEditorClassAttribute(mathML, this.customEditors.getActiveEditor().toolbar);
+      if (this.customEditors.getActiveEditor() !== null) {
+        const { toolbar } = this.customEditors.getActiveEditor();
+        mathML = MathML.addCustomEditorClassAttribute(mathML, toolbar);
       } else {
         // We need - if exists - the editor name from MathML
         // class attribute.
-        for (const key in this.customEditors.editors) {
+        Object.keys(this.customEditors.editors).forEach((key) => {
           mathML = MathML.removeCustomEditorClassAttribute(mathML, key);
-        }
+        });
       }
       const mathmlEntitiesEncoded = MathML.mathMLEntities(mathML);
       this.integrationModel.updateFormula(mathmlEntitiesEncoded);
-    }
-    else {
+    } else {
       this.integrationModel.updateFormula(null);
     }
 
@@ -358,26 +355,24 @@ export default class ContentManager {
       } else {
         this.setMathML('<math><semantics><annotation encoding="application/json">[]</annotation></semantics></math>', true);
       }
+    } else if (this.editor.getEditorModel().isRTL()) {
+      this.setMathML('<math dir="rtl"/>', true);
     } else {
-      if (this.editor.getEditorModel().isRTL()) {
-        this.setMathML('<math dir="rtl"/>', true);
-      } else {
-        this.setMathML('<math/>', true);
-      }
+      this.setMathML('<math/>', true);
     }
   }
 
   /**
    * Open event. Triggered by {@link ModalDialog.open}. Does the following:
-   * - Updates the {@link ContentManager.editor} content (with an empty MathML or an existing formula),
+   * - Updates the {@link ContentManager.editor} content
+   *   (with an empty MathML or an existing formula),
    * - Updates the {@link ContentManager.editor} toolbar.
    * - Recovers the the focus.
    */
   onOpen() {
     if (this.isNewElement) {
       this.setEmptyMathML();
-    }
-    else {
+    } else {
       this.setMathML(this.mathML);
     }
     this.updateToolbar();
@@ -385,51 +380,55 @@ export default class ContentManager {
   }
 
   /**
-   * Sets the correct toolbar depending if exist other custom toolbars at the same time (e.g: Chemistry).
+   * Sets the correct toolbar depending if exist other custom toolbars
+   * at the same time (e.g: Chemistry).
    */
   updateToolbar() {
     this.updateTitle(this.modalDialogInstance);
-    var customEditor;
-    if (customEditor = this.customEditors.getActiveEditor()) {
-      var toolbar = customEditor.toolbar ? customEditor.toolbar : _wrs_int_wirisProperties['toolbar'];
-      if (this.toolbar == null || this.toolbar != toolbar) {
+    const customEditor = this.customEditors.getActiveEditor();
+    if (customEditor) {
+      const toolbar = customEditor.toolbar
+        ? customEditor.toolbar
+        : _wrs_int_wirisProperties.toolbar;
+
+      if (this.toolbar == null || this.toolbar !== toolbar) {
         this.setToolbar(toolbar);
       }
     } else {
-      var toolbar = this.getToolbar();
-      if (this.toolbar == null || this.toolbar != toolbar) {
+      const toolbar = this.getToolbar();
+      if (this.toolbar == null || this.toolbar !== toolbar) {
         this.setToolbar(toolbar);
         this.customEditors.disable();
       }
     }
   }
+
   /**
    * Updates the current {@link ModalDialog.title}. If a {@link CustomEditors} is enabled
    * sets the custom editor title. Otherwise sets the default title.
    */
   updateTitle() {
-    var customEditor;
-    if (customEditor = this.customEditors.getActiveEditor()) {
+    const customEditor = this.customEditors.getActiveEditor();
+    if (customEditor) {
       this.modalDialogInstance.setTitle(customEditor.title);
     } else {
       this.modalDialogInstance.setTitle('MathType');
     }
   }
+
   /**
    * Returns the editor toolbar, depending on the configuration local or server side.
    * @returns {String} - Toolbar identifier.
    */
   getToolbar() {
-    var toolbar;
-
+    let toolbar = 'general';
     if ('toolbar' in this.editorAttributes) {
-      toolbar = this.editorAttributes.toolbar;
-    } else {
-      toolbar = "general";
+      ({ toolbar } = this.editorAttributes.toolbar);
     }
     // TODO: Change global integration variable for integration custom toolbar.
-    if (toolbar == 'general') {
-      toolbar = (typeof _wrs_int_wirisProperties == 'undefined' || typeof _wrs_int_wirisProperties['toolbar'] == 'undefined') ? 'general' : _wrs_int_wirisProperties['toolbar'];
+    if (toolbar === 'general') {
+      // eslint-disable-next-line camelcase
+      toolbar = (typeof _wrs_int_wirisProperties === 'undefined' || typeof _wrs_int_wirisProperties.toolbar === 'undefined') ? 'general' : _wrs_int_wirisProperties.toolbar;
     }
 
     return toolbar;
@@ -441,7 +440,7 @@ export default class ContentManager {
    */
   setToolbar(toolbar) {
     this.toolbar = toolbar;
-    this.editor.setParams({ 'toolbar': this.toolbar });
+    this.editor.setParams({ toolbar: this.toolbar });
   }
 
   /**
@@ -462,11 +461,11 @@ export default class ContentManager {
       // Code to detect Esc event.
       if (keyboardEvent.key === 'Escape' || keyboardEvent.key === 'Esc') {
         // There should be only one element with class name 'wrs_pressed' at the same time.
-        const list = document.getElementsByClassName('wrs_expandButton wrs_expandButtonFor3RowsLayout wrs_pressed');
+        let list = document.getElementsByClassName('wrs_expandButton wrs_expandButtonFor3RowsLayout wrs_pressed');
         if (list.length === 0) {
-          const list = document.getElementsByClassName('wrs_expandButton wrs_expandButtonFor2RowsLayout wrs_pressed');
+          list = document.getElementsByClassName('wrs_expandButton wrs_expandButtonFor2RowsLayout wrs_pressed');
           if (list.length === 0) {
-            const list = document.getElementsByClassName('wrs_select wrs_pressed');
+            list = document.getElementsByClassName('wrs_select wrs_pressed');
             if (list.length === 0) {
               this.modalDialogInstance.cancelAction();
               keyboardEvent.stopPropagation();
@@ -474,20 +473,17 @@ export default class ContentManager {
             }
           }
         }
-      }
-      // Code to detect shift Tab event.
-      else if (keyboardEvent.shiftKey && keyboardEvent.key === 'Tab') {
-        if (document.activeElement == this.modalDialogInstance.submitButton) {
+      } else if (keyboardEvent.shiftKey && keyboardEvent.key === 'Tab') { // Code to detect shift Tab event.
+        if (document.activeElement === this.modalDialogInstance.submitButton) {
           this.editor.focus();
           keyboardEvent.stopPropagation();
           keyboardEvent.preventDefault();
-        }
-        else {
+        } else {
           // There should be more than one element with class name 'wrs_selected'.
-          let list = document.getElementsByClassName('wrs_selected');
+          const list = document.getElementsByClassName('wrs_selected');
           let done = false;
-          for (let i = 0; !done && i < list.length; i++) {
-            if (list[i].getAttribute('role') == 'tab' && document.activeElement == list[i]) {
+          for (let i = 0; !done && i < list.length; i += 1) {
+            if (list[i].getAttribute('role') === 'tab' && document.activeElement === list[i]) {
               this.modalDialogInstance.cancelButton.focus();
               done = true;
               keyboardEvent.stopPropagation();
@@ -495,25 +491,22 @@ export default class ContentManager {
             }
           }
         }
-      }
-      // Code to detect Tab event.
-      else if (keyboardEvent.key === 'Tab') {
-        if (document.activeElement == this.modalDialogInstance.cancelButton) {
+      } else if (keyboardEvent.key === 'Tab') { // Code to detect Tab event.
+        if (document.activeElement === this.modalDialogInstance.cancelButton) {
           const list = document.getElementsByClassName('wrs_selected');
           let done = false;
-          for (let i = 0; !done && i < list.length; i++) {
-            if (list[i].getAttribute('role') == 'tab') {
+          for (let i = 0; !done && i < list.length; i += 1) {
+            if (list[i].getAttribute('role') === 'tab') {
               list[i].focus();
               done = true;
               keyboardEvent.stopPropagation();
               keyboardEvent.preventDefault();
             }
           }
-        }
-        else {
+        } else {
           // There should be only one element with class name 'wrs_formulaDisplay'.
           const element = document.getElementsByClassName('wrs_formulaDisplay')[0];
-          if (element.getAttribute('class') == 'wrs_formulaDisplay wrs_focused') {
+          if (element.getAttribute('class') === 'wrs_formulaDisplay wrs_focused') {
             this.modalDialogInstance.submitButton.focus();
             keyboardEvent.stopPropagation();
             keyboardEvent.preventDefault();
