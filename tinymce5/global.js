aabe82b912a77a4a41e6ade75e541f2d2de04179
Manuel Cagigas <manuel@wiris.com>
2019-06-18 11:01:07 +0200

    PLUGINS-1223: Folder file for TinyMCE5

---
+++ tinymce5/global.js
@@ -0,0 +1,27 @@
+import Core from './integration-js/src/core.src.js';
+import Parser from './integration-js/src/parser.js';
+import Util from './integration-js/src/util.js';
+import Image from './integration-js/src/image.js';
+import Configuration from './integration-js/src/configuration.js';
+import Listeners from './integration-js/src/listeners';
+import IntegrationModel from './integration-js/src/integrationmodel.js';
+import { TinyMceIntegration, currentInstance, instances } from './editor_plugin.src.js';
+import Latex from './integration-js/src/latex';
+import backwardsLib from './integration-js/src/backwardsLib';
+import Test from './integration-js/src/test';
+
+// Expose WirisPlugin variable to the window.
+window.WirisPlugin = {
+    Core: Core,
+    Parser: Parser,
+    Image: Image,
+    Util: Util,
+    Configuration: Configuration,
+    Listeners: Listeners,
+    IntegrationModel: IntegrationModel,
+    currentInstance: currentInstance,
+    instances: instances,
+    TinyMceIntegration: TinyMceIntegration,
+    Latex: Latex,
+    Test : Test
+}
