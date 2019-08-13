fdd62082bf7ad2ae9e32560f22f2fb706c6f59da
Xavier Ripoll <xavier@wiris.com>
2019-08-13 10:21:29 +0200

    PLUGINS-1254: Switched tag closing algorithm to a simpler approach

--- packages/ckeditor5-mathml/src/conversion/mathmldataprocessor.js
+++ packages/ckeditor5-mathml/src/conversion/mathmldataprocessor.js
@@ -72,7 +72,7 @@ export default class MathmlDataProcessor extends XmlDataProcessor {
         data = data.replace( /[\u00A0-\u9999]/gm, i => '&#' + i.charCodeAt( 0 ) + ';' );
 
         // We need to find and close the dangling open tags like <img> that HTML5 allows but XML does not.
-        data = closeTags( data );
+        data = data.replace( /<(?:area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)[\s\S]*?>/g, match => match.replace( /([^\/])>/, '$1/>' ) );
 
         // Convert input XML data to DOM DocumentFragment.
         const domFragment = this._toDom( data );
@@ -82,73 +82,4 @@ export default class MathmlDataProcessor extends XmlDataProcessor {
 
     }
 
-}
-
-/**
- * Closes the dangling open tags
- * @param {string} markup string containing the markup to be processed
- * @returns {string} the string with the dangling open tags actually closed
- */
-function closeTags( markup ) {
-    // markup = '<a>xxx<b>yyy<c>zzz</b>www</a>ttt'
-    // [\s\S] acts like (.|\n), as there is no multiline matching flag in js
-    const tags = markup.match( /<[\s\S]*?>/g ); // [ '<a>', '<b>', '<c>', '</b>', '</a>' ]
-    const text = markup.split( /<[\s\S]*?>/g ); // [ '', 'xxx', 'yyy', 'zzz', 'www', 'ttt' ]
-
-    // Now, we're going to close the dangling open tags.
-    // First, for each tag we generate a triple [index : number, name : string, type : number]
-    // type: 0 = open, 1 = close, 2 = self-closing
-    let data = tags.map( ( tag, index ) => [
-        index,
-        tag.match( /[\w-]+/ )[ 0 ],
-        /\/>/.test( tag )
-            ? 2 // self-closing
-            : /<\//.test( tag )
-            ? 1 // close
-            : 0 // open
-    ] );
-
-    data = data.filter( ( [ , , type ] ) => type != 2 ); // Remove self-closed tags as they don't affect others
-
-    for ( const index of findDangling( data ) ) {
-        tags[ index ] = tags[ index ].replace( '>', '/>' );
-    }
-
-    return text
-        .map( ( piece, index ) => [ piece, tags[ index ] ] ) // [ [ '', '<a>' ], [ 'xxx', '<b>' ], [ 'yyy', '<c/>' ], [ 'zzz', '</b>' ], [ 'www', '</a>' ], [ 'ttt', undefined ] ]
-        // Use reduce + concat instead of flat because Edge doesn't support flat
-        .reduce( ( acc, cur ) => acc.concat( cur ), [] ) // [ '', '<a>', 'xxx', '<b>', 'yyy', '<c/>', 'zzz', '</b>', 'www', '</a>', 'ttt', undefined ]
-        .filter( piece => typeof piece !== 'undefined' ) // [ '', '<a>', 'xxx', '<b>', 'yyy', '<c/>', 'zzz', '</b>', 'www', '</a>', 'ttt' ]
-        .reduce( ( acc, cur ) => acc + cur ); // '<a>xxx<b>yyy<c/>zzz</b>www</a>ttt'
-}
-
-/**
- * Finds the dangling open tags in the indicated subarray of data
- * @param {string} data array consisting of [index, name, type] tuples
- * @param {number} start First index of the subarray
- * @param {number} end Index after the last element of the subarray
- * @param {number[]} dangling Indexes of the tags to fix; leave empty in first call
- */
-function findDangling( data, start = 0, end = data.length, dangling = [] ) {
-    let i = start;
-    const endIndex = end != data.length ? data[ end ][ 0 ] : data[ data.length - 1 ][ 0 ] + 1;
-    while ( i < end ) {
-        let [ openIndex, openName, ] = data[ i ];
-        if ( data[ i ][ 2 ] == 0 ) { // open tag, as it should be if the HTML is properly formed
-            const j = data.findIndex( ( [ closeIndex, closeName, closeType ] ) => (
-                openIndex < closeIndex &&
-                closeIndex < endIndex &&
-                closeName == openName &&
-                closeType == 1
-            ) ) || [];
-            if ( j == -1 ) { // No closing tag, fix
-                dangling.push( openIndex );
-                i++;
-            } else {
-                dangling = findDangling( data, i + 1, j, dangling );
-                i = j + 1;
-            }
-        }
-    }
-    return dangling;
 }
