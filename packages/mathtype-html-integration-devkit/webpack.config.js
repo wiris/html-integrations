3427fce86740904404a0054333b94928d334a3eb
Xavier Ripoll <xavier@wiris.com>
2019-10-29 10:04:39 +0100

    PLUGINS-1270: Add raw-loader to integration

--- packages/mathtype-html-integration-devkit/webpack.config.js
+++ packages/mathtype-html-integration-devkit/webpack.config.js
@@ -41,7 +41,13 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
+                // For the modal close, minimize, maximize icons
+                test: /styles\/icons\/[^\/]+\/[^\/]+\.svg$/,
+                use: [ 'raw-loader' ]
+            },
+            {
                 test: /\.(png|ttf|otf|eot|svg|woff(2)?)(.*)?$/,
+                exclude: /styles\/icons\/[^\/]+\/[^\/]+\.svg$/,
                 use: [
                   {
                     loader: 'url-loader',
