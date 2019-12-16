fcffd8cb68560afcdb92326074f6c7303c6c6c63
Xavier Ripoll <xavier@wiris.com>
2019-12-16 12:09:54 +0100

    PLUGINS-1287: Use loaders on CSS files in TinyMCE plugins

--- packages/mathtype-tinymce4/webpack.config.js
+++ packages/mathtype-tinymce4/webpack.config.js
@@ -36,7 +36,6 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: /node_modules/,
                 use: ['style-loader', 'css-loader']
             },
             {
