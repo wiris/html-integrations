50f9e0278c054c8f92c8b02b9ae01ccbdcf49ed1
Carla Lara <carla@wiris.com>
2020-02-14 08:04:16 +0100

    PLUGINS-1311: Renamed some files.

--- demos/froala3/webpack.config.js
+++ demos/froala3/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'generic.js'
+        filename: 'demo.js'
     },
     devServer: {
         writeToDisk: true,
