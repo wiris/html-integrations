9336188a9456d508192d83d29e34683ce7a2e3ed
Carla Lara <carla@wiris.com>
2020-02-17 11:41:15 +0100

    PLUGINS-1311: Added lerna dependency.

--- demos/ckeditor4/global.js
+++ demos/ckeditor4/global.js
@@ -1,10 +1,9 @@
+// Load scripts
+import '@wiris/mathtype-ckeditor4/plugin.js';
+
+
 // Add wiris plugin
 CKEDITOR.plugins.addExternal('ckeditor_wiris', './node_modules/@wiris/mathtype-ckeditor4/', 'plugin.js');
-
-// Change editor configuration
-CKEDITOR.editorConfig = function (config) {
-    
-};
 
 // Initialize plugin
 CKEDITOR.replace( 'editor', {
--- demos/froala3/global.js
+++ demos/froala3/global.js
@@ -1,3 +1,6 @@
+// Load scripts
+import '@wiris/mathtype-froala3/wiris.js';
+
 // Load styles 
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 
