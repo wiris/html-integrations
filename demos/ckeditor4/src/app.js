4ac18f8e07ff66aa6b215c87e9a12fb39b32fdd7
Carla Lara <carla@wiris.com>
2020-02-21 11:27:17 +0100

    PLUGINS-1311: Added wiris tool to transform the editor content into image content.

--- demos/ckeditor4/src/app.js
+++ demos/ckeditor4/src/app.js
@@ -1,5 +1,11 @@
+// Generate scripts
+var jsDemoImagesTransform = document.createElement('script');
+jsDemoImagesTransform.type = 'text/javascript';
+jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+
 // Load scripts
 import '@wiris/mathtype-ckeditor4/plugin';
+document.head.appendChild(jsDemoImagesTransform);
 
 // Load styles
 import './static/style.css';
@@ -12,4 +18,23 @@ CKEDITOR.plugins.addExternal('ckeditor_wiris', `${window.location.href}node_modu
 CKEDITOR.replace('editor', {
   extraPlugins: 'ckeditor_wiris',
   allowedContent: true,
+});
+
+// Takes the data of the editor
+// Replaces the content of a div with the data transformed
+function updateFunction() {
+  const editorContent = CKEDITOR.instances.editor.getData();
+  document.getElementById("editorContentTransform").innerHTML = editorContent;
+  com.wiris.js.JsPluginViewer.parseElement(document.getElementById('editorContentTransform'));
+}
+
+// Add listener on click button to launch updateFunction
+document.getElementById('btn-update').addEventListener('click', ()=>{
+  updateFunction();
+});
+
+// Execute, just for the first time, 
+// the transformation of the actual CKEDITOR4 content
+CKEDITOR.on('instanceReady', function() {
+  updateFunction();
 });
