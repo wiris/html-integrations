c40eea2cb28d65e8938e98a6892de0646f39fd48
Daniel Canet <dcanet@wiris.com>
2020-03-04 17:45:54 +0100

    Add markup and logic to make the plugin work

--- demos/react-froala3/src/index.js
+++ demos/react-froala3/src/index.js
@@ -1,18 +1,4 @@
 // Default React App from create-react-app command
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -29,13 +15,14 @@ import $ from 'jquery';
 window.$ = $;
 
 window.FroalaEditor = require('froala-editor');
-require('@wiris/mathtype-froala3');
  
-const toolbar = ['wirisEditor', 'wirisChemistry'];
+require('@wiris/mathtype-froala3');
+
+const toolbar = ['undo', 'redo', 'bold', '|', 'wirisEditor', 'wirisChemistry', 'clear', 'insert'];
 const froalaConfig = {
   iframe: true,
   charCounterCount: false,
-  imageEditButtons: ['wirisEditor', 'wirisChemistry', 'imageRemove'],
+  imageEditButtons: ['wirisEditor', 'wirisChemistry', 'imageDisplay', 'imageAlign', 'imageInfo', 'imageRemove'],
   toolbarButtons: toolbar,
   toolbarButtonsMD: toolbar,
   toolbarButtonsSM: toolbar,
@@ -46,10 +33,23 @@ const froalaConfig = {
   imageResize : false,
   key: 'CA5D-16E3A2E3G1I4A8B8A9B1D2rxycF-7b1C3vyz==',
   heightMax: 310,
-  useClasses: false
+  useClasses: false,
+  // Execute on initialyzed editor
+  events: {
+    initialized: function (e) {
+      // Get froala and wiris plugin versions.
+      // const versionWiris = WirisPlugin.currentInstance.version;             //eslint-disable-line
+      const versionFroala = FroalaEditor.VERSION;                           //eslint-disable-line
+      // document.getElementById('version-wiris').innerHTML += versionWiris;
+      document.getElementById('version-froala').innerHTML += versionFroala;
+
+      // const editor = this;
+      // console.log('@@@@@@initialized', editor, e);
+    }
+  }
 };
- 
-ReactDOM.render(<FroalaEditorComponent config={ froalaConfig } />, document.getElementById('root'));
+
+ReactDOM.render(<FroalaEditorComponent config={ froalaConfig } />, document.getElementById('editor'));
  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
