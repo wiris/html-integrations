ce5577f22a43a8738b6a6dc1a4604784670a944e
Carla Lara <carla@wiris.com>
2020-03-26 13:40:00 +0100

    PLUGINS-1329: Created a demo to test the changes applied on the ckeditor 4 package

---
+++ demos/issue69kanban/src/app.js
@@ -0,0 +1,52 @@
+// Load scripts.
+import '@wiris/mathtype-ckeditor4/plugin';
+
+// Load styles.
+import './static/style.css';
+
+// Generate scripts.
+const jsDemoImagesTransform = document.createElement('script');
+jsDemoImagesTransform.type = 'text/javascript';
+jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+
+// Load generated scripts.
+document.head.appendChild(jsDemoImagesTransform);
+
+// Copy the editor content before initializing it.
+document.getElementById('transform_content').innerHTML = document.getElementById('editor').innerHTML;
+
+// Add wiris plugin.
+CKEDITOR.plugins.addExternal('ckeditor_wiris', `${window.location.href}node_modules/@wiris/mathtype-ckeditor4/`, 'plugin.js'); //eslint-disable-line
+
+// Initialize plugin.
+CKEDITOR.replace('editor', { //eslint-disable-line
+  extraPlugins: 'ckeditor_wiris',
+  allowedContent: true,
+  toolbar: [
+    { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike'] },
+    { name: 'clipboard', items: ['Undo', 'Redo'] },
+    { name: 'wirisplugins', items: ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_formulaEditorChemistry'] },
+  ],
+});
+
+// Handle on editor ready event
+CKEDITOR.on('instanceReady', function() {                     //eslint-disable-line
+  // Get ckeditor and wiris plugin versions.
+  const versionWiris = WirisPlugin.currentInstance.version;             //eslint-disable-line
+  const versionCKeditor = CKEDITOR.version;                             //eslint-disable-line
+  document.getElementById('version_wiris').innerHTML += versionWiris;
+  document.getElementById('version_ckeditor').innerHTML += versionCKeditor;
+});
+
+// Takes the data of the editor.
+// Replaces the content of a div with the data transformed.
+function updateFunction() {
+  const editorContent = CKEDITOR.instances.editor.getData(); //eslint-disable-line
+  document.getElementById('transform_content').innerHTML = editorContent;
+  com.wiris.js.JsPluginViewer.parseElement(document.getElementById('transform_content')); //eslint-disable-line
+}
+
+// Add listener on click button to launch updateFunction.
+document.getElementById('btn_update').addEventListener('click', () => {
+  updateFunction();
+});
