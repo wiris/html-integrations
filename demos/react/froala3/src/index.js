b3b2ffe1f3fe0a1683ba03dd7b59ebc0757e317f
Carla Lara <carla@wiris.com>
2020-05-13 19:20:09 +0200

    PLUGINS-1332: Add initial content on froala+react demo

--- demos/react/froala3/src/index.js
+++ demos/react/froala3/src/index.js
@@ -78,8 +78,11 @@ const froalaConfig = {
   }
 };
 
+// Set the initial content
+const content = '<p class="text"> Double click on the following formula to edit it.</p><p style="text-align:center;"><math><mi>z</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>3</mn></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math></p>'
+
 // Render our components on page.
-ReactDOM.render(<FroalaEditorComponent config={ froalaConfig } />, document.getElementById('editor'));
+ReactDOM.render(<FroalaEditorComponent config={ froalaConfig } model={ content } />, document.getElementById('editor'));
  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
