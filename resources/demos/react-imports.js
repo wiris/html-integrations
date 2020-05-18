235484f920b8771ca06fda061f47b4fbbf61e852
Carla Lara <carla@wiris.com>
2020-05-18 12:20:03 +0200

    PLUGINS-1332: Create a js file specific to be used for the react demos

---
+++ resources/demos/react-imports.js
@@ -0,0 +1,13 @@
+// Import styles.
+import './design.css';
+
+// Export generic functions.
+export * from './common.js';
+
+// Generate scripts.
+const jsDemoImagesTransform = document.createElement('script');
+jsDemoImagesTransform.type = 'text/javascript';
+jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+
+// Load generated scripts.
+document.head.appendChild(jsDemoImagesTransform);
