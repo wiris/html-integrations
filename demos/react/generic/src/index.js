14bdb49dd1ef48d8f61169b404af4d7bc3a452f6
Carla Lara <carla@wiris.com>
2020-05-18 13:16:51 +0200

    PLUGINS-1332: Add styles and others to react+generic demo

--- demos/react/generic/src/index.js
+++ demos/react/generic/src/index.js
@@ -3,55 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+// Import wiris dependencies.
 import { wrsInitEditor } from '@wiris/mathtype-generic/wirisplugin-generic.src';
 import '@wiris/mathtype-generic/wirisplugin-generic';
+import { version as pluginVersion } from '@wiris/mathtype-generic/package.json';
 
-// Load WIRISplugins.js dinamically
-const jsDemoImagesTransform = document.createElement('script');
-jsDemoImagesTransform.type = 'text/javascript';
-jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
-// Load generated scripts.
-document.head.appendChild(jsDemoImagesTransform);
+// Load the file that contains common imports between demos. (functions, styles, etc)
+import * as Generic from 'resources/demos/react-imports';
+
+// Set the initial content.
+const content = '<p class="text"> Double click on the following formula to edit it.</p><p style="text-align:center;"><math><mi>z</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>3</mn></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math></p>'
+
+// Copy the editor content before initializing it.
+document.getElementById('transform_content').innerHTML = content;
+
+document.getElementById('version_wiris').innerHTML += pluginVersion;
+
+// Add listener on click button to launch updateContent function.
+document.getElementById('btn_update').addEventListener('click', () => {
+  Generic.updateContent(WirisPlugin.Parser.initParse(editableDiv.innerHTML), 'transform_content');      //eslint-disable-line
+});
 
 
-function Toolbar(props) {
+// Create toolbar component.
+function Toolbar() {
   return (
-    <div id="toolbar" key={props.id}></div>
+    <div id="toolbar"></div>
   );
 }
 
+// Create editable component.
 function HtmlEditor(props) {
   return (
-    <div id="htmlEditor" key={props.id} contentEditable="true" dangerouslySetInnerHTML={{ __html: props.data }}></div>
+    <div id="htmlEditor" contentEditable="true" dangerouslySetInnerHTML={{ __html: props.data }}></div>
   );
 }
 
 class Editor extends React.Component {
   componentDidMount() {
-    // Load the toolbar and the editable area into const variables to work easy with them
+    // Load the toolbar and the editable area into const variables to work easy with them.
     const editableDiv = document.getElementById('htmlEditor');
     const toolbarDiv = document.getElementById('toolbar');
-    console.log(editableDiv);
-    console.log(toolbarDiv)
 
     // Initialyze the editor.
     wrsInitEditor(editableDiv, toolbarDiv);
   }
   render() {
+    // Array of react components.
     return [
-      <Toolbar id = "toolbar"/>,
-      <HtmlEditor data = {content} id="htmlEditor"/>
+      <Toolbar />,
+      <HtmlEditor data = {content} />
     ];
   }
 }
 
-const content = '<p class="text"> Double click on the following formula to edit it.</p><p style="text-align:center;"><math><mi>z</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>3</mn></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math></p>';
-
-
-
-ReactDOM.render(
-  <Editor key="ff"/>,
-  document.getElementById('root')
+ReactDOM.render(<Editor />, document.getElementById('editor')
 );
 
 // If you want your app to work offline and load faster, you can change
