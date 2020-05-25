8a251af8ca9cc58d6cfe54b2d10897f3d3a2969e
Carla Lara <carla@wiris.com>
2020-05-25 19:23:54 +0200

    Copy demo react+froala3 from plugins-1332

--- demos/react/froala3/src/index.js
+++ demos/react/froala3/src/index.js
@@ -13,35 +13,32 @@ import 'froala-editor/js/plugins.pkgd.min.js';
 // Add jquery.
 import $ from 'jquery';
 
-// This needs to be included before the '@wiris/mathtype-froala3' is loaded synchronously
+// Load the file that contains common imports between demos. (functions, styles, etc)
+import * as Generic from 'resources/demos/react-imports';
+
+// Import the wiris plugin version.
+import { version as pluginVersion } from '@wiris/mathtype-froala3/package.json';
+
+// This needs to be included before the '@wiris/mathtype-froala3' is loaded synchronously.
 window.$ = $;
 window.FroalaEditor = require('froala-editor');
 
-// Load scripts synchronously.
+// Load scripts synchronously..
 require('@wiris/mathtype-froala3');
 
-// Generate scripts.
-const jsDemoImagesTransform = document.createElement('script');
-jsDemoImagesTransform.type = 'text/javascript';
-jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+// Apply specific demo names to all the objects.
+document.getElementById('header_title_name').innerHTML = 'Mathtype for Froala';
+document.getElementById('version_editor').innerHTML = 'Froala editor: ';
 
-// Load generated scripts.
-document.head.appendChild(jsDemoImagesTransform);
+// Set the initial content.
+const content = '<p class="text"> Double click on the following formula to edit it.</p><p style="text-align:center;"><math><mi>z</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>3</mn></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math></p>'
 
 // Copy the editor content before initializing it.
-document.getElementById('editorContentTransform').innerHTML = document.getElementById('editor').innerHTML;
-
-// Takes the data of the editor.
-// Replaces the content of a div with the data transformed.
-function updateFunction() {
-  const editorContent = FroalaEditor.INSTANCES[0].html.get();                                           //eslint-disable-line
-  document.getElementById('editorContentTransform').innerHTML = editorContent;
-  com.wiris.js.JsPluginViewer.parseElement(document.getElementById('editorContentTransform'));                //eslint-disable-line
-}
+document.getElementById('transform_content').innerHTML = content;
 
 // Add listener on click button to launch updateFunction.
-document.getElementById('btn-update').addEventListener('click', () => {
-  updateFunction();
+document.getElementById('btn_update').addEventListener('click', () => {
+  Generic.updateContent(FroalaEditor.INSTANCES[0].html.get(), 'transform_content');                     //eslint-disable-line
 });
 
 // Initialize editor.
@@ -55,31 +52,34 @@ const froalaConfig = {
   htmlAllowedTags: ['.*'],
   htmlAllowedAttrs: ['.*'],
 
-  // htmlAllowedEmptyTags: ['mprescripts'],
-  // imageResize : false,
-  // key: 'CA5D-16E3A2E3G1I4A8B8A9B1D2rxycF-7b1C3vyz==',
+  htmlAllowedEmptyTags: ['mprescripts'],
+  imageResize : false,
+  key: 'CA5D-16E3A2E3G1I4A8B8A9B1D2rxycF-7b1C3vyz==',
   // heightMax: 310,
   // useClasses: false,
 
-  // Execute on initialyzed editor
+  // Execute on initialyzed editor.
   events: {
     initialized: function (e) {
-      // Get froala and wiris plugin versions.
-      // const versionWiris  = WirisPlugin.currentInstance.version;             //eslint-disable-line
-      const versionFroala = FroalaEditor.VERSION;                           //eslint-disable-line
-
-      // document.getElementById('version-wiris').innerHTML += versionWiris;
-      document.getElementById('version-froala').innerHTML += versionFroala;
-
-      // const editor = this;
-      // console.log('@@@@@@initialized', editor, e);
-
+      // Get and set the editor and wiris versions in this order.
+      Generic.setEditorAndWirisVersion(FroalaEditor.VERSION, pluginVersion);        //eslint-disable-line
     }
   }
 };
 
+/* Create a component to be rendered later.
+ This is important to remove complexity from the reactDom.render 
+ and to be able to add other functionality. */
+class Editor extends React.Component {
+  render() {
+    return (
+      <FroalaEditorComponent config={ froalaConfig } model={ content } />
+    );
+  }
+}
+
 // Render our components on page.
-ReactDOM.render(<FroalaEditorComponent config={ froalaConfig } />, document.getElementById('editor'));
+ReactDOM.render(<Editor />, document.getElementById('editor'));
  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
