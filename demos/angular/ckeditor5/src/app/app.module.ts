4df7277904652bfdf40f93bfc3abfff82de864aa
Carla Lara <carla@wiris.com>
2020-05-25 16:02:43 +0200

    Copy demo angular+ckeditor5 from plugins-1332

---
+++ demos/angular/ckeditor5/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CKEditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
