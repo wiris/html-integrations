4993333e21a5a36ef33e930b1ea62da800c2e710
Carla Lara <carla@wiris.com>
2020-05-25 12:04:35 +0200

    PLUGINS-1332: Modify the resources folder to treat it as a package. Add some new scripts for the react and angular demos

---
+++ resources/demos/angular-imports.js
@@ -0,0 +1,10 @@
+// Export generic functions.
+export * from './common.js';
+
+// Generate scripts.
+const jsDemoImagesTransform = document.createElement('script');
+jsDemoImagesTransform.type = 'text/javascript';
+jsDemoImagesTransform.src = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+
+// Load generated scripts.
+document.head.appendChild(jsDemoImagesTransform);
