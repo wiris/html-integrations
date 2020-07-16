ea938ba3f04edf088390021b9e2083520d5e3d08
Carla Lara <carla@wiris.com>
2020-07-16 16:16:53 +0200

    KB-3048: Modify the scripts when runint the devkit tests to execute by block the npm i and then the jest command from the root folder and sincronous while other blocks run their comands. Add the sript torun the file that contains the tests

--- scripts/services/executeTests.js
+++ scripts/services/executeTests.js
@@ -4,29 +4,58 @@ const { exec } = require('child_process');
 const process = require('process');
 
 /**
- * Executes all the tests of the project in paralel.
+ * Install all the dependencies of the entire project
+ * to be able to execute tests later.
  */
-const runTests = () => new Promise((resolve, reject) => {
-  // html-integration-devkit
-  const src = './packages/mathtype-html-integration-devkit/';
-  const srcNormalized = path.normalize(src);
-  process.chdir(srcNormalized); // Go to the srcNormalized dir
-  // exec('npm install', (error, stdout, stderr) => {
-  //   console.log('stdout: ' + stdout);  // eslint-disable-line
-  //   console.log('stderr: ' + stderr);  // eslint-disable-line
-  //   if (error !== null) {
-  //     console.log('exec error: ' + error); // eslint-disable-line
-  //   }
-  // });
-  exec('jest', (err, stdout, stderr) => {
+const installDeps = route => new Promise((resolve, reject) => {
+  exec(`npm install --prefix ${path.normalize(route)}`, (err, stdout, stderr) => {
     if (err) {
       reject(err);
     }
-    resolve(stderr);
+    resolve({ dout: stdout, derr: stderr });
   });
 });
 
-if (!module.parent) { // This file is being executed as a script.
+/**
+ * Executes all the tests of the project in paralel.
+ */
+const runTests = route => new Promise((resolve, reject) => {
+  // html-integration-devkit
+  exec(`jest --config=${route}jest.config.js ${route}`, (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+    }
+    resolve({ dout: stdout, derr: stderr });
+  });
+});
+
+/**
+ * Executes the tests and the install for the devkit package.
+ * The run in sequence as the install must be done before executing the tests.
+ */
+const devkit = route => Promise.resolve(
+  installDeps(route).then((installOut) => {
+    console.log(installOut);  //eslint-disable-line
+    runTests(route).then((testsOut) => {
+    console.log(testsOut);  //eslint-disable-line
+    });
+  }),
+);
+
+/**
+ * Execute all the tests and installs of the project.
+ */
+const executeTests = () => new Promise((resolve) => {
+  const paths = require('./paths.json'); // eslint-disable-line global-require
+  resolve(
+    Promise.all([
+      devkit(paths.devkit),
+    ]),
+  );
+});
+
+// This file is being executed as a script.
+if (!module.parent) {
   // Process args
   const args = process.argv.slice(2);
 
@@ -35,9 +64,8 @@ if (!module.parent) { // This file is being executed as a script.
     emitWarning('No parameters needed, all the additional parameters will be ignored.');
   }
 
-  runTests().then((data) => {
-    console.log(data);  //eslint-disable-line
-  });
+  // Execute all the tests and resolve when finished
+  executeTests();
 } else { // This file is being imported as a module.
   module.exports = runTests;
 }
