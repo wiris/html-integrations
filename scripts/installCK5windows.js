7d77729200300fedfefb15823410495a339b0440
Daniel Canet <dcanet@wiris.com>
2021-02-23 14:00:29 +0100

    Lint command for "npm run build-dev-windows"

--- scripts/installCK5windows.js
+++ scripts/installCK5windows.js
@@ -1,44 +1,43 @@
 const { exec } = require('child_process');
 
 const pack = () => new Promise((resolve, reject) => {
-    exec(`cd packages/mathtype-ckeditor5/ && npm pack --quiet `, (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        }
-        console.log({ dout: stdout, derr: stderr })
-        resolve({ dout: stdout, derr: stderr });
-    });
+  exec('cd packages/mathtype-ckeditor5/ && npm pack --quiet ', (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+    }
+    console.log({ dout: stdout, derr: stderr });
+    resolve({ dout: stdout, derr: stderr });
+  });
 });
 
-const  install_mathtype = (path) => new Promise((resolve, reject) => {
-    exec(`cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`, (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        }
-        console.log({ dout: stdout, derr: stderr })
-        resolve({ dout: stdout, derr: stderr });
-    });
+const install_mathtype = (path) => new Promise((resolve, reject) => {
+  exec(`cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`, (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+    }
+    console.log({ dout: stdout, derr: stderr });
+    resolve({ dout: stdout, derr: stderr });
+  });
 });
 
 const sequenceExecution = () => Promise.resolve(
-    pack().then((packOut) => {
-      install_mathtype(packOut.dout);
-    }),
-  );
+  pack().then((packOut) => {
+    install_mathtype(packOut.dout);
+  }),
+);
 
 // This file is being executed as a script.
 if (!module.parent) {
-    // Process args
-    const args = process.argv.slice(2);
-  
-    // Log a warning if there are more than 0 arguments
-    if (args.length > 0) {
-      emitWarning('No parameters needed, all the additional parameters will be ignored.');
-    }
-  
-    // Execute all the tests and resolve when finished
-    sequenceExecution();
-  } else { // This file is being imported as a module.
-    module.exports = sequenceExecution;
+  // Process args
+  const args = process.argv.slice(2);
+
+  // Log a warning if there are more than 0 arguments
+  if (args.length > 0) {
+    emitWarning('No parameters needed, all the additional parameters will be ignored.');
   }
-  
+
+  // Execute all the tests and resolve when finished
+  sequenceExecution();
+} else { // This file is being imported as a module.
+  module.exports = sequenceExecution;
+}
