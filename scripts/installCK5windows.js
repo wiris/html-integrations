886bd12cf6265c56d98ee07dba9dabdc6434ac32
Carla Lara <carla@wiris.com>
2021-05-10 11:12:07 +0200

    Remove eslint ignores from scripts folder
    New rules have been added while linting other project files,
    So some of the eslint ignores existing in the scripts folder
    have to be deleted as the lines don't throw errors
    
    #taskid 16000

--- scripts/installCK5windows.js
+++ scripts/installCK5windows.js
@@ -1,33 +1,42 @@
 const { exec } = require('child_process');
 const { emitWarning } = require('process');
 
-const pack = () => new Promise((resolve, reject) => {
-  exec('cd packages/mathtype-ckeditor5/ && npm pack --quiet ', (err, stdout, stderr) => {
-    if (err) {
-      reject(err);
-    }
-    // eslint-disable-next-line no-console
-    console.log({ dout: stdout, derr: stderr });
-    resolve({ dout: stdout, derr: stderr });
+const pack = () =>
+  new Promise((resolve, reject) => {
+    exec(
+      'cd packages/mathtype-ckeditor5/ && npm pack --quiet ',
+      (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        }
+        // eslint-disable-next-line no-console
+        console.log({ dout: stdout, derr: stderr });
+        resolve({ dout: stdout, derr: stderr });
+      }
+    );
   });
-});
 
-const installMathtype = (path) => new Promise((resolve, reject) => {
-  exec(`cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`, (err, stdout, stderr) => {
-    if (err) {
-      reject(err);
-    }
-    // eslint-disable-next-line no-console
-    console.log({ dout: stdout, derr: stderr });
-    resolve({ dout: stdout, derr: stderr });
+const installMathtype = (path) =>
+  new Promise((resolve, reject) => {
+    exec(
+      `cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`,
+      (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        }
+        // eslint-disable-next-line no-console
+        console.log({ dout: stdout, derr: stderr });
+        resolve({ dout: stdout, derr: stderr });
+      }
+    );
   });
-});
 
-const sequenceExecution = () => Promise.resolve(
-  pack().then((packOut) => {
-    installMathtype(packOut.dout);
-  }),
-);
+const sequenceExecution = () =>
+  Promise.resolve(
+    pack().then((packOut) => {
+      installMathtype(packOut.dout);
+    })
+  );
 
 // This file is being executed as a script.
 if (!module.parent) {
@@ -36,11 +45,14 @@ if (!module.parent) {
 
   // Log a warning if there are more than 0 arguments
   if (args.length > 0) {
-    emitWarning('No parameters needed, all the additional parameters will be ignored.');
+    emitWarning(
+      'No parameters needed, all the additional parameters will be ignored.'
+    );
   }
 
   // Execute all the tests and resolve when finished
   sequenceExecution();
-} else { // This file is being imported as a module.
+} else {
+  // This file is being imported as a module.
   module.exports = sequenceExecution;
 }
