54de6bd29b265b7906b6af6657201e2889f4da16
Carla Lara <carla@wiris.com>
2021-05-10 13:31:50 +0200

    Remove lint commands from demos
    #taskid 16000

--- scripts/installCK5windows.js
+++ scripts/installCK5windows.js
@@ -1,42 +1,39 @@
 const { exec } = require('child_process');
 const { emitWarning } = require('process');
 
-const pack = () =>
-  new Promise((resolve, reject) => {
-    exec(
-      'cd packages/mathtype-ckeditor5/ && npm pack --quiet ',
-      (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        }
-        // eslint-disable-next-line no-console
-        console.log({ dout: stdout, derr: stderr });
-        resolve({ dout: stdout, derr: stderr });
+const pack = () => new Promise((resolve, reject) => {
+  exec(
+    'cd packages/mathtype-ckeditor5/ && npm pack --quiet ',
+    (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
       }
-    );
-  });
-
-const installMathtype = (path) =>
-  new Promise((resolve, reject) => {
-    exec(
-      `cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`,
-      (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        }
-        // eslint-disable-next-line no-console
-        console.log({ dout: stdout, derr: stderr });
-        resolve({ dout: stdout, derr: stderr });
-      }
-    );
-  });
-
-const sequenceExecution = () =>
-  Promise.resolve(
-    pack().then((packOut) => {
-      installMathtype(packOut.dout);
-    })
+      // eslint-disable-next-line no-console
+      console.log({ dout: stdout, derr: stderr });
+      resolve({ dout: stdout, derr: stderr });
+    },
   );
+});
+
+const installMathtype = (path) => new Promise((resolve, reject) => {
+  exec(
+    `cd demos/html5/ckeditor5 && npm install ../../../packages/mathtype-ckeditor5/${path}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      }
+      // eslint-disable-next-line no-console
+      console.log({ dout: stdout, derr: stderr });
+      resolve({ dout: stdout, derr: stderr });
+    },
+  );
+});
+
+const sequenceExecution = () => Promise.resolve(
+  pack().then((packOut) => {
+    installMathtype(packOut.dout);
+  }),
+);
 
 // This file is being executed as a script.
 if (!module.parent) {
@@ -46,7 +43,7 @@ if (!module.parent) {
   // Log a warning if there are more than 0 arguments
   if (args.length > 0) {
     emitWarning(
-      'No parameters needed, all the additional parameters will be ignored.'
+      'No parameters needed, all the additional parameters will be ignored.',
     );
   }
 
