c692271c3ecfbfaf9b8f1a2a14080664d332092c
Daniel Canet <dcanet@wiris.com>
2021-05-11 15:54:10 +0200

    Lint html: remove 'element-permitted-content'

--- .htmlvalidate.js
+++ .htmlvalidate.js
@@ -4,11 +4,11 @@ module.exports = {
     ],
 
     "rules": {
-      "close-order": "error", // Throws error if the closure is bad: wrong order, not closed, closed but not initialyzed, etc.
-      
-      // Let "bad" content to be inside the incorrect brackets. This is because demos need to have the tag script inside html
-      // TODO: Study a better way to put this in order to now shuting down all the unpermited other contents
-      "element-permitted-content": "off",
-      "void-style": "off" // Disables having some elements to be stricted with the closure for react html. TODO: Study a better wat to do it in react apps and delete the rule
+      // Throws error if the closure is bad: wrong order, not closed, closed but 
+      // not initialyzed, etc.
+      "close-order": "error",      
+      // Disables having some elements to be stricted with the closure for react html.
+      // TODO: Study a better wat to do it in react apps and delete the rule.
+      "void-style": "off"
     }
   }
