cf6209ae7fa2b232bf9822f6a84ecbad39bbd655
carla-at-wiris <carla@wiris.com>
2023-07-13 16:50:30 +0200

    test: Add more unit test for removeSemantics
    #taskid 36632

--- packages/devkit/test/mathml.test.js
+++ packages/devkit/test/mathml.test.js
@@ -16,3 +16,23 @@ test('Validate Math not Hand formula', () => {
 test('Validate Chem not Hand formula', () => {
   expect(MathML.removeSemantics(constants.chemNoMrow)).toBe(constants.chemNoMrowValid);
 });
+
+test('Validate Multiple semantics Hand formula 01', () => {
+  expect(MathML.removeSemantics(constants.semantics01)).toBe(constants.semanticsValid01);
+});
+
+test('Validate Multiple semantics Hand formula 02', () => {
+  expect(MathML.removeSemantics(constants.semantics02)).toBe(constants.semanticsValid02);
+});
+
+test('Validate Multiple semantics Hand formula 03', () => {
+  expect(MathML.removeSemantics(constants.semantics03)).toBe(constants.semanticsValid03);
+});
+
+test('Validate Multiple semantics Hand formula 04', () => {
+  expect(MathML.removeSemantics(constants.semantics04)).toBe(constants.semanticsValid04);
+});
+
+test('Validate formula without annotation and semantics', () => {
+  expect(MathML.removeSemantics(constants.noSemanticsNoAnnotation)).toBe(constants.noSemanticsNoAnnotation);
+});
