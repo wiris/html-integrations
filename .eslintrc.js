3d763e1964615c05c9dc1ee6409d76ce1b2c6f3b
Jose González <78553646+jgonzalez-at-wiris@users.noreply.github.com>
2024-04-12 11:41:09 +0200

    fix: Update actions .yml
    Update JSDocs workflow node version to avoid problems with documentation generation.
    Update Publish and Deploy Staging node versions to the latest LTS: 20.
    
    taskid #45473

--- .eslintrc.js
+++ .eslintrc.js
@@ -133,16 +133,6 @@ module.exports = {
         ActiveXObject: 'readonly',
       },
     },
-    // 02. Custom rules for Cypress Test implementation
-    // This extension only adds specific rules for cypress that need to be combined with the airbnb ones
-    // {
-    //   files: [
-    //     'cypress/**/*.js',
-    //   ],
-    //   extends: [
-    //     'plugin:cypress/recommended',
-    //   ],
-    // },
     // 03. Custom rules for the packages with jsDoc support.
     {
       files: [
