3974c807c7eb8931729f4d901b7ac9bdcd65a448
Xinyu Jiang <125269853+xjiang-at-wiris@users.noreply.github.com>
2024-08-26 12:20:07 +0200

    chore: create test brancj

--- packages/res/demos/imports.js
+++ packages/res/demos/imports.js
@@ -7,11 +7,11 @@ export * from "./common.js";
 
 // Display html content.
 // We force the use of html-loader, for angular and react applications.
-const htmlModule = require("html-loader!./index.html");
+// const htmlModule = require("html-loader!./index.html");
 
 // Since html-loader versions 2.0.0, it returns a module with its default valude beeing the html.
-const htmlBody = htmlModule.default;
-document.body.innerHTML = htmlBody;
+// const htmlBody = htmlModule.default;
+// document.body.innerHTML = htmlBody;
 
 // Generate scripts.
 const jsDemoImagesTransform = document.createElement("script");
