5e0b4f5d10e15c1f9cd439d4f719a2ae025c4f47
Carla Lara <60604864+carla-at-wiris@users.noreply.github.com>
2025-03-13 11:18:39 +0100

    fix: Export CK5 Wiris classes (#1084)

--- packages/ckeditor5/src/index.js
+++ packages/ckeditor5/src/index.js
@@ -1,3 +1,6 @@
 import MathType from "./plugin.js";
+import CKEditor5Integration from "./integration.js";
+import { MathTypeCommand, ChemTypeCommand } from "./commands.js";
 
 export default MathType;
+export { CKEditor5Integration, MathTypeCommand, ChemTypeCommand };
