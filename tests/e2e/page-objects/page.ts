e732c6f847691b510e5971ce29a051ed55e2c1b6
sbaron-at-wiris <sergi.baron@wiris.com>
2026-03-31 11:54:57 +0200

    test: add tests for track changes

--- tests/e2e/page-objects/page.ts
+++ tests/e2e/page-objects/page.ts
@@ -13,4 +13,12 @@ export default class BasePage {
   async pause(milliseconds: number): Promise<void> {
     await this.page.waitForTimeout(milliseconds)
   }
+
+  public async press(key: string): Promise<void> {
+    await this.page.keyboard.press(key)
+  }
+
+  public async type(text: string): Promise<void> {
+    await this.page.keyboard.type(text)
+  }
 }
