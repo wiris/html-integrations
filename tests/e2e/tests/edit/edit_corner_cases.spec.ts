d21ed228c67db8852f86766d18c99c9548e0754d
sbaron-at-wiris <sergi.baron@wiris.com>
2026-05-06 16:26:56 +0200

    test: add corner test cases for edit and insert

--- tests/e2e/tests/edit/edit_corner_cases.spec.ts
+++ tests/e2e/tests/edit/edit_corner_cases.spec.ts
@@ -12,7 +12,7 @@ for (const editorName of editors) {
   }, () => {
     test(`MTHTML-81 Edit styled equation: ${editorName} editor`, async ({ page }) => {
       const { editor, wirisEditor } = await setupEditor(page, editorName)
-      
+
       await editor.open()
       await editor.clear()
       await editor.openWirisEditor(Toolbar.MATH)
@@ -31,9 +31,35 @@ for (const editorName of editors) {
       expect(isEquationPresent).toBeTruthy()
     })
 
+    test(`MTHTML-97 Validate formula edition on styled text blocks: ${editorName} editor`, async ({ page }) => {
+      const { editor, wirisEditor } = await setupEditor(page, editorName)
+
+      await editor.open()
+      await editor.clear()
+      await editor.applyStyle()
+      await editor.type('Styled  text')
+      await editor.press('ArrowLeft', { times: 5 })
+      await editor.openWirisEditor(Toolbar.MATH)
+      await wirisEditor.waitUntilLoaded()
+      await wirisEditor.insertEquationUsingEntryForm(Equations.singleNumber.mathml)
+      await editor.waitForEquation(Equations.singleNumber)
+
+      await editor.openWirisEditorForLastInsertedFormula(Toolbar.MATH, Equations.singleNumber)
+      await wirisEditor.waitUntilLoaded()
+      await wirisEditor.typeEquationViaKeyboard('+1')
+      await wirisEditor.insertButton.click()
+      await wirisEditor.waitUntilClosed()
+
+      const equationsInHTMLEditor = await editor.getEquations()
+      const isEquationPresent = equationsInHTMLEditor.some((equation: Equation) => equation.altText === Equations.OnePlusOne.altText)
+      const isTextStyled = await editor.isTextBoldAndItalic('Styled') && await editor.isTextBoldAndItalic('text')
+      expect(isEquationPresent).toBeTruthy()
+      expect(isTextStyled).toBeTruthy()
+    })
+
     test(`MTHTML-85 User edits a formula and continues typing text: ${editorName} editor`, async ({ page }) => {
       const { editor, wirisEditor } = await setupEditor(page, editorName)
-      
+
       await editor.open()
       await editor.clear()
       await editor.openWirisEditor(Toolbar.MATH)
@@ -59,7 +85,7 @@ for (const editorName of editors) {
 
     test(`MTHTML-100 User edits a formula deleted during edition: ${editorName} editor`, async ({ page }) => {
       const { editor, wirisEditor } = await setupEditor(page, editorName)
-      
+
       await editor.open()
       await editor.clear()
       await editor.openWirisEditor(Toolbar.MATH)
@@ -76,7 +102,7 @@ for (const editorName of editors) {
 
       // No error should be visible, and the editor should be blank
       expect(await editor.isEditorCleared()).toBeTruthy()
-      
+
       // Again, we do an insert + edit so we assure everything works as expected
       await editor.openWirisEditor(Toolbar.MATH)
       await wirisEditor.waitUntilLoaded()
@@ -87,6 +113,30 @@ for (const editorName of editors) {
       await wirisEditor.waitUntilLoaded()
       await wirisEditor.pause(1000)
       await wirisEditor.insertEquationViaKeyboard('+1')
+
+      const equationsInHTMLEditor = await editor.getEquations()
+      const isEquationPresent = equationsInHTMLEditor.some((equation: Equation) => equation.altText === Equations.OnePlusOne.altText)
+      expect(isEquationPresent).toBeTruthy()
+    })
+
+    test(`MTHTML-9 User edits formula via contextual toolbar: ${editorName} editor`, async ({ page }) => {
+      const { editor, wirisEditor } = await setupEditor(page, editorName)
+      test.skip(editorName !== 'froala', `This test is only relevant for the Froala editor, as it's the only one with a contextual toolbar`)
+
+      await editor.open()
+      await editor.clear()
+      await editor.openWirisEditor(Toolbar.MATH)
+      await wirisEditor.waitUntilLoaded()
+      await wirisEditor.insertEquationUsingEntryForm(Equations.singleNumber.mathml)
+      await editor.waitForEquation(Equations.singleNumber)
+
+      // Click on the equation to open the contextual toolbar and click the edit button
+      await editor.openWirisEditorForLastInsertedFormula(Toolbar.MATH, Equations.singleNumber)
+      await wirisEditor.waitUntilLoaded()
+      await wirisEditor.typeEquationViaKeyboard('+1')
+      await wirisEditor.insertButton.click()
+      await wirisEditor.waitUntilClosed()
+      await editor.waitForEquation(Equations.OnePlusOne)
 
       const equationsInHTMLEditor = await editor.getEquations()
       const isEquationPresent = equationsInHTMLEditor.some((equation: Equation) => equation.altText === Equations.OnePlusOne.altText)
